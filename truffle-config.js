004771d9b4b701d2aa938a3b0599ad0403298f09
Alice Henshaw <henshawalice@gmail.com>
2020-03-03 14:12:14 -0500

    setup truffle and ganache

---
+++ truffle-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // contracts_directory: './flatten',
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*',
+      gas: 0x90F560,
+      gasPrice: 0x01,
+    }
+  }
+}
