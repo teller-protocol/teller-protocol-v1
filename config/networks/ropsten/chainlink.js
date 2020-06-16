f51ebdb6a5d5040bf36d8fda972e7ccf40db930c
Guillermo Daniel Salazar <guillesalazar@gmail.com>
2020-06-16 17:50:43 -0300

    Fixing USD/LINK issue.

--- config/networks/ropsten/chainlink.js
+++ config/networks/ropsten/chainlink.js
@@ -2,6 +2,5 @@
 module.exports = {
     USDC_ETH: '0xE1480303DDe539E2c241bdC527649F37c9cBef7d',
     DAI_ETH: '0x64b8e49baDeD7BFb2FD5A9235B2440C0eE02971B',
-    // TODO The current USD/LINK Oracle is not working on Ropsten
-    USD_LINK: '0x060b38B197fE60cA5F36EA94452DA7F1bc3c7823',
+    USD_LINK: '0xc21c178fE75aAd2017DA25071c54462e26d8face',
 };
