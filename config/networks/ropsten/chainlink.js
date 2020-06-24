7d347caa25c0e682560387c08394a865cca6e0c6
Guillermo Daniel Salazar <guillesalazar@gmail.com>
2020-06-24 15:21:20 -0300

    Adding logic to use Inverse oracle when it is needed.

--- config/networks/ropsten/chainlink.js
+++ config/networks/ropsten/chainlink.js
@@ -2,16 +2,19 @@
 module.exports = {
     USDC_ETH: {
         address: '0xE1480303DDe539E2c241bdC527649F37c9cBef7d',
+        inversed: false,
         collateralDecimals: 18, // ETH
         responseDecimals: 18,
     },
     DAI_ETH: {
         address: '0x64b8e49baDeD7BFb2FD5A9235B2440C0eE02971B',
+        inversed: false,
         collateralDecimals: 18, // ETH
         responseDecimals: 18,
     },
     LINK_USD: {
         address: '0xc21c178fE75aAd2017DA25071c54462e26d8face',
+        inversed: true,
         collateralDecimals: 18, // ETH
         responseDecimals: 8,
     },
--- config/networks/test/chainlink.js
+++ config/networks/test/chainlink.js
@@ -3,16 +3,19 @@ const { DUMMY_ADDRESS } = require('../../consts');
 module.exports = {
     USDC_ETH: {
         address: DUMMY_ADDRESS,
+        inversed: false,
         collateralDecimals: 18, // ETH
         responseDecimals: 18,
     },
     DAI_ETH: {
         address: DUMMY_ADDRESS,
+        inversed: false,
         collateralDecimals: 18, // ETH
         responseDecimals: 18,
     },
     LINK_USD: {
         address: DUMMY_ADDRESS,
+        inversed: true,
         collateralDecimals: 18, // LINK
         responseDecimals: 8,
     },
