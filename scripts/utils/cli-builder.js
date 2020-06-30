4306d0af55d5005a35517e100e27aecc7db69a90
Guillermo Daniel Salazar <guillesalazar@gmail.com>
2020-06-30 09:40:50 -0300

    Changing truffle scripts to support cli params.

--- scripts/utils/cli-builder.js
+++ scripts/utils/cli-builder.js
@@ -72,6 +72,88 @@ module.exports = {
             return yargs;
         },
     },
+    tokens: {
+        mint: () => {
+            newOption(
+                yargs,
+                'tokenName',
+                'TN',
+                'string',
+                'Token name to mint.',
+                undefined,
+            );
+            newOption(
+                yargs,
+                'receiverIndex',
+                'RI',
+                'number',
+                'Address (index) that will receive the tokens.',
+                0,
+            );
+            newOption(
+                yargs,
+                'amount',
+                'A',
+                'number',
+                'Amount to mint. Default: 10000',
+                10000,
+            );
+            return yargs;
+        },
+        balanceOf: () => {
+            newOption(
+                yargs,
+                'tokenName',
+                'TN',
+                'string',
+                'Token name to mint.',
+                undefined,
+            );
+            newOption(
+                yargs,
+                'accountIndex',
+                'AI',
+                'number',
+                'Address (index) to verify the balance.',
+                0,
+            );
+
+            return yargs;
+        },
+    },
+    ganache: {
+        advanceTime: () => {
+            newOption(
+                yargs,
+                'network',
+                'N',
+                'string',
+                'Sets the network to use in the execution.',
+                undefined,
+            );
+            newOption(
+                yargs,
+                'seconds',
+                'S',
+                'number',
+                'Seconds to advance. Default: 120 seconds',
+                60 * 2,
+            );
+            return yargs;
+        },
+        setOraclePrice: () => {
+            loansBase(yargs);
+            newOption(
+                yargs,
+                'newValue',
+                'SI',
+                'number',
+                'New value.',
+                undefined,
+            );
+            return yargs;
+        },
+    },
     loans: {
         getAllLoansFor: () => {
             addLoansBase(yargs);
@@ -158,6 +240,51 @@ module.exports = {
             addNewValue(yargs);
             addSettingName(yargs);
             addSenderIndex(yargs);
+            return yargs;
+        },        
+    },
+    cTokens: () => {
+        newOption(
+            yargs,
+            'cTokenName',
+            'CTN',
+            'string',
+            'CToken name to get the exchange rate.',
+            undefined,
+        );
+        return yargs;
+    },
+    settings: {
+        view: () => {
+            base(yargs);
+            return yargs;
+        },
+        setNewSetting: (defaultValue = 10) => {
+            base(yargs);
+            newOption(
+                yargs,
+                'newValue',
+                'NV',
+                'string',
+                'New value to set. Time is in seconds.',
+                defaultValue,
+            );
+            newOption(
+                yargs,
+                'settingName',
+                'SN',
+                'string',
+                'Setting name to connfigure.',
+                undefined,
+            );
+            newOption(
+                yargs,
+                'senderIndex',
+                'SI',
+                'string',
+                `Sender index account. By default is 0`,
+                0,
+            );
             return yargs;
         },
     },
