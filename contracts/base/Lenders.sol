2a09bafa3031181bed0759e407831751c586ece7
Guillermo Daniel Salazar <guillesalazar@gmail.com>
2020-07-16 10:21:52 -0300

    Fixing high/medium issues.

--- contracts/base/Lenders.sol
+++ contracts/base/Lenders.sol
@@ -88,6 +88,7 @@ contract Lenders is Base, LendersInterface {
         ZeroCollateralCommon.InterestResponse[] calldata responses
     ) external isInitialized() whenNotPaused() whenLendingPoolNotPaused(lendingPool) {
         require(
+            accruedInterest[request.lender].timeLastAccrued == 0 ||
             accruedInterest[request.lender].timeLastAccrued == request.startTime,
             "GAP_IN_INTEREST_ACCRUAL"
         );
