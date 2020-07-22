b47907beef1f9948c3f4989def9c977e5ee4b223
Noah Passalacqua <noah.passa@gmail.com>
2020-07-22 12:28:07 -0400

    Add docs for contracts IV.

--- contracts/interfaces/ZTokenInterface.sol
+++ contracts/interfaces/ZTokenInterface.sol
@@ -4,11 +4,27 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 
 /**
-    Note: since interfaces cannot be inherited by other interfaces, this ZTokenInterface is defined as contract.
-    See details at: https://github.com/ethereum/solidity/issues/3419#issuecomment-429988401
- */
+ * @notice This contract represents a wrapped token within the Teller protocol
+ * @dev Note: since interfaces cannot be inherited by other interfaces, this ZTokenInterface is defined as contract.
+ * See details at: https://github.com/ethereum/solidity/issues/3419#issuecomment-429988401
+ *
+ * @author develop@teller.finance
+*/
+// TODO: Change to TToken
 contract ZTokenInterface is IERC20 {
+    /**
+     * @notice Reduce account supply of specified token amount
+     * @param account The account to burn tokens from
+     * @param amount The amount of tokens to burn
+     * @return true if successful
+     */
     function burn(address account, uint256 amount) external returns (bool);
 
+    /**
+     * @notice Increase account supply of specified token amount
+     * @param account The account to mint tokens for
+     * @param amount The amount of tokens to mint
+     * @return true if successful
+     */
     function mint(address account, uint256 amount) external returns (bool);
 }
