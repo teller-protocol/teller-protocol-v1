58922cb7d502429faae36cfee34e4a8355742b3a
Guillermo Daniel Salazar <guillesalazar@gmail.com>
2020-07-23 15:25:16 -0300

    Changing unit tests.

--- test/base/EtherCollateralLoansGetBorrowerLoansTest.js
+++ test/base/EtherCollateralLoansGetBorrowerLoansTest.js
@@ -4,10 +4,8 @@ const abi = require('ethereumjs-abi')
 const {
   t,
   NULL_ADDRESS,
-  TERMS_SET,
   THIRTY_DAYS
 } = require('../utils/consts');
-const { loans } = require('../utils/events');
 const { createLoanRequest, createUnsignedLoanResponse } = require('../utils/structs');
 
 // Mock contracts
@@ -18,7 +16,7 @@ const Loans = artifacts.require("./mock/base/EtherCollateralLoansMock.sol");
 const Settings = artifacts.require("./base/Settings.sol");
 const LoanTermsConsensus = artifacts.require("./base/LoanTermsConsensus.sol");
 
-contract('EtherCollateralLoansCreateLoanWithTermsTest', function (accounts) {
+contract('EtherCollateralLoansGetBorrowerLoansTest', function (accounts) {
     let instance;
     let loanTermsConsInstance;
     let lendingPoolInstance;
@@ -60,24 +58,15 @@ contract('EtherCollateralLoansCreateLoanWithTermsTest', function (accounts) {
     });
 
     withData({
-        _1_no_msg_value: [3, 0, 0, undefined, false],
-        _2_with_msg_value: [17, 500000, 500000, undefined, false],
-        _3_msg_value_collateral_param_not_match_1: [17, 0, 500000, 'INCORRECT_ETH_AMOUNT', true],
-        _4_msg_value_collateral_param_not_match_2: [17, 500000, 0, 'INCORRECT_ETH_AMOUNT', true],
-        _5_msg_value_collateral_param_not_match_3: [17, 200000, 200001, 'INCORRECT_ETH_AMOUNT', true],
-    }, function(
-        mockLoanIDCounter,
-        initialCollateral,
-        msgValue,
-        expectedErrorMessage,
-        mustFail
-    ) {    
-        it(t('user', 'createLoanWithTerms', 'Should able (or not) to set loan terms.', mustFail), async function() {
-            const interestRate = Math.floor((responseOne.interestRate + responseTwo.interestRate) / 2)
-            const collateralRatio = Math.floor((responseOne.collateralRatio + responseTwo.collateralRatio) / 2)
-            const maxLoanAmount = Math.floor((responseOne.maxLoanAmount + responseTwo.maxLoanAmount) / 2)
+        _1_valid_no_msg_value: [3, 0],
+        _2_valid_with_msg_value: [17, 500000],
+    }, function(mockLoanIDCounter, msgValue) {
+        it(t('user', 'getBorrowerLoans', 'Should able to get the borrower loan ids.', false), async function() {
+            const interestRate = Math.floor((responseOne.interestRate + responseTwo.interestRate) / 2);
+            const collateralRatio = Math.floor((responseOne.collateralRatio + responseTwo.collateralRatio) / 2);
+            const maxLoanAmount = Math.floor((responseOne.maxLoanAmount + responseTwo.maxLoanAmount) / 2);
 
-            await instance.setLoanIDCounter(mockLoanIDCounter)
+            await instance.setLoanIDCounter(mockLoanIDCounter);
 
             // mock consensus response
             await loanTermsConsInstance.givenMethodReturn(
@@ -86,72 +75,23 @@ contract('EtherCollateralLoansCreateLoanWithTermsTest', function (accounts) {
                     ['uint256', 'uint256', 'uint256'],
                     [interestRate.toString(), collateralRatio.toString(), maxLoanAmount.toString()]
                 )
-            )
+            );
+            await instance.createLoanWithTerms(
+                loanRequest,
+                [responseOne, responseTwo],
+                msgValue,
+                {
+                    from: borrowerAddress,
+                    value: msgValue
+                }
+            );
 
-            const totalBefore = await instance.totalCollateral.call()
-            const contractBalBefore = await web3.eth.getBalance(instance.address)
+            // Invocation
+            const borrowedLoans = await instance.getBorrowerLoans(borrowerAddress);
 
-            try {
-                // Invocation
-                const tx = await instance.createLoanWithTerms(
-                    loanRequest,
-                    [responseOne, responseTwo],
-                    initialCollateral,
-                    {
-                        from: borrowerAddress,
-                        value: msgValue
-                    }
-                );
-
-                assert(!mustFail, 'It should have failed because data is invalid.');
-                assert(tx);
-
-                const txTime = (await web3.eth.getBlock(tx.receipt.blockNumber)).timestamp
-                const termsExpiry = txTime + THIRTY_DAYS
-                const lastCollateralIn = msgValue == 0 ? 0 : txTime
-
-                const totalAfter = await instance.totalCollateral.call()
-                const contractBalAfter = await web3.eth.getBalance(instance.address)
-
-                const loan = await instance.loans.call(mockLoanIDCounter)
-
-                assert.equal(loan['id'].toString(), mockLoanIDCounter)
-                assert.equal(loan['loanTerms']['borrower'].toString(), loanRequest.borrower)
-                assert.equal(loan['loanTerms']['recipient'].toString(), loanRequest.recipient)
-                assert.equal(loan['loanTerms']['interestRate'].toString(), interestRate)
-                assert.equal(loan['loanTerms']['collateralRatio'].toString(), collateralRatio)
-                assert.equal(loan['loanTerms']['maxLoanAmount'].toString(), maxLoanAmount)
-                assert.equal(loan['loanTerms']['duration'].toString(), loanRequest.duration)
-                assert.equal(loan['termsExpiry'].toString(), termsExpiry)
-                assert.equal(loan['loanStartTime'].toString(), 0)
-                assert.equal(loan['collateral'].toString(), msgValue)
-                assert.equal(loan['lastCollateralIn'].toString(), lastCollateralIn)
-                assert.equal(loan['principalOwed'].toString(), 0)
-                assert.equal(loan['interestOwed'].toString(), 0)
-                assert.equal(loan['status'].toString(), TERMS_SET)
-                assert.equal(loan['liquidated'], false)
-
-
-                assert.equal(parseInt(totalBefore) + msgValue, parseInt(totalAfter))
-                assert.equal(parseInt(contractBalBefore) + msgValue, parseInt(contractBalAfter))
-
-                loans
-                    .loanTermsSet(tx)
-                    .emitted(
-                        mockLoanIDCounter, 
-                        loanRequest.borrower,
-                        loanRequest.recipient,
-                        interestRate,
-                        collateralRatio,
-                        maxLoanAmount,
-                        loanRequest.duration,
-                        txTime + THIRTY_DAYS
-                    )
-            } catch (error) {
-                assert(mustFail);
-                assert(error);
-                assert.equal(error.reason, expectedErrorMessage);
-            }
+            // Assertions
+            assert.equal(borrowedLoans[0].toString(), mockLoanIDCounter.toString());
+            assert.equal(borrowedLoans.length, 1);
         });
     });
 });
