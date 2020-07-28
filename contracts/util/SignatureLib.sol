0f7bf2d4c1f12774a698d194f86d90acefea222d
sypeer <sy.peeran@gmail.com>
2020-07-28 07:48:25 -0400

    adding docs

--- contracts/util/SignatureLib.sol
+++ contracts/util/SignatureLib.sol
@@ -16,6 +16,7 @@ import "../util/ZeroCollateralCommon.sol";
         @notice Generates a hash for the loan response
         @param response Structs of the protocol loan responses
         @param requestHash Hash of the loan request
+        @param chainId Current chain id
         @return bytes32 Hash of the loan response
      */
     function hashLoanTermsResponse(
@@ -41,6 +42,8 @@ import "../util/ZeroCollateralCommon.sol";
     /**
         @notice Generates a hash for the loan request
         @param request Struct of the protocol loan request
+        @param callerAddress Address of the caller
+        @param chainId Current chain id
         @return bytes32 Hash of the loan request
      */
     function hashLoanTermsRequest(
@@ -72,6 +75,7 @@ import "../util/ZeroCollateralCommon.sol";
         @notice It creates a hash based on a node response and lender request.
         @param response a node response.
         @param requestHash a hash value that represents the lender request.
+        @param chainId Current chain id
         @return a hash value.
      */
     function hashInterestResponse(
@@ -95,6 +99,8 @@ import "../util/ZeroCollateralCommon.sol";
     /**
         @notice It creates a hash value based on the lender request.
         @param request the interest request sent by the lender.
+        @param callerAddress Address of the caller
+        @param chainId Current chain id
         @return a hash value.
      */
     function hashInterestRequest(
