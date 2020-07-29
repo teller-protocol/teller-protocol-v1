51c57df413eda5f926361c9621b28f7219bcb7f2
Noah Passalacqua <noah.passa@gmail.com>
2020-07-29 14:28:02 -0500

    Create EscrowFactory contract.

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -72,6 +72,8 @@ module.exports = async function(deployer, network, accounts) {
     { ...networkConfig, escrowFactory, txConfig, network, currentBlockNumber, web3 },
     { ERC20 },
   );
+  await deployerApp.deploy(Escrow)
+  await deployerApp.deploy(EscrowFactory, Escrow.address)
 
   await deployerApp.deploy(
     ATMSettings,
