b5b25561ac84eb53cf76ccc195e7e1104a4cb41c
sypeer <sy.peeran@gmail.com>
2020-08-04 19:18:16 -0400

    deployed to rinkeby

--- config/networks/rinkeby/chainlink.js
+++ config/networks/rinkeby/chainlink.js
@@ -1,6 +1,21 @@
-const { DUMMY_ADDRESS } = require('../../consts');
 
 module.exports = {
-    USDC_ETH: DUMMY_ADDRESS,
-    DAI_ETH: DUMMY_ADDRESS,
+    USDC_ETH: {
+        address: '0xdE54467873c3BCAA76421061036053e371721708',
+        inversed: false,
+        collateralDecimals: 18, // ETH
+        responseDecimals: 18,
+    },
+    DAI_ETH: {
+        address: '0x037E8F2125bF532F3e228991e051c8A7253B642c',
+        inversed: false,
+        collateralDecimals: 18, // ETH
+        responseDecimals: 18,
+    },
+    LINK_USD: {
+        address: '0x32dbd3214aC75223e27e575C53944307914F7a90',
+        inversed: true,
+        collateralDecimals: 18, // ETH
+        responseDecimals: 8,
+    },
 };
--- config/networks/rinkeby/index.js
+++ config/networks/rinkeby/index.js
@@ -2,6 +2,9 @@ module.exports = {
     network: 'rinkeby',
     chainlink: require('./chainlink'),
     compound: require('./compound'),
+    tokens: require('./tokens'),
+    zerocollateral: require('./zerocollateral'),
+    assetSettings: require('./assetSettings'),
     maxGasLimit: 7000000,
     toTxUrl: ({ tx }) => {
         return `https://rinkeby.etherscan.io/tx/${tx}`;
