630b3fe1eabb6b644bc50a2989e991944d2a897c
Guillermo Daniel Salazar <guillesalazar@gmail.com>
2020-08-05 18:02:27 -0300

    Removing empty lines.

--- config/networks/rinkeby/index.js
+++ config/networks/rinkeby/index.js
@@ -4,7 +4,6 @@ module.exports = {
     compound: require('./compound'),
     assetSettings: require('./assetSettings'),
     nodeComponentsVersions: require('./nodeComponentsVersions'),
-
     maxGasLimit: 7000000,
     toTxUrl: ({ tx }) => {
         return `https://rinkeby.etherscan.io/tx/${tx}`;
