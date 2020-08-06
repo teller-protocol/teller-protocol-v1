37af96ab2ea9aec5e4140eb0ab151af8b7827c3a
Guillermo Daniel Salazar <guillesalazar@gmail.com>
2020-08-06 09:33:38 -0300

    Fixing unit tests.

--- test/base/BaseWhenAllowedTest.js
+++ test/base/BaseWhenAllowedTest.js
@@ -1,6 +1,7 @@
 // JS Libraries
 const withData = require('leche').withData;
 const { t } = require('../utils/consts');
+const { createTestSettingsInstance } = require('../utils/settings-helper');
 
 // Mock contracts
 const BaseMock = artifacts.require("./mock/base/BaseMock.sol");
@@ -14,7 +15,7 @@ contract('BaseWhenAllowedTest', function (accounts) {
     let instance;
 
     beforeEach('Setup for each test', async () => {
-        settings = await Settings.new(1, 1, 1, 1, 1, 1, 1, 1);
+        settings = await createTestSettingsInstance(Settings);
         instance = await BaseMock.new();
         await instance.externalInitialize(settings.address);
     });
