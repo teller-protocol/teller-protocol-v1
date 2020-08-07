d9696b55e934b015b09d9bb10cca8124947e5c5b
sypeer <sy.peeran@gmail.com>
2020-08-07 15:59:46 -0400

    Adding ATM Token contract

---
+++ contracts/base/governance/ATMToken.sol
@@ -0,0 +1,155 @@
+pragma solidity ^0.5.17;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+import "@openzeppelin/contracts/lifecycle/Pausable.sol";
+
+/**
+  *  @title ATM Test Token for Teller DAO
+  *  @author developer@teller.finance
+ */
+
+contract ATMTestToken is ERC20Detailed, Pausable, ERC20Mintable {
+    /**
+      *  @dev ATMTestToken implements an ERC20 token with a supply cap and a vesting scheduling
+     */
+
+    // State Variables
+    uint256 private _cap;
+
+    // Struct
+    struct VestingTokens {
+        address beneficiary;
+        uint256 amount;
+        uint256 deadline;
+    }
+
+    // Mappings
+    mapping (address => VestingTokens) private _vestingBalances;
+
+    // Events
+    event NewCap(uint256 newcap);
+    event NewVesting(address beneficiary, uint256 amount, uint256 deadline);
+    event VestingClaimed(address beneficiary, uint256 amount);
+    event RevokeVesting(address beneficiary, uint256 amount, uint256 deadline);
+
+    // Constructor
+    constructor (
+        uint256 cap
+    ) public
+      ERC20Detailed("ATMToken", "ATM", 18)
+    {
+        require(cap > 0, "ERC20Capped: cap is 0");
+        _cap = cap;
+    }
+
+    // Functions
+    /**
+     * @dev Returns the cap on the token's total supply.
+     */
+    function cap() public view returns (uint256) {
+        return _cap;
+    }
+
+    /**
+     * @dev Sets a new cap on the token's total supply.
+     * @param newcap The new capped amount of tokens
+     */
+    function setCap(uint256 newcap) external onlyPauser() whenNotPaused() {
+        _cap = newcap;
+        emit NewCap(_cap);
+    }
+
+    /**
+     * @notice Increase account supply of specified token amount
+     * @param account The account to mint tokens for
+     * @param amount The amount of tokens to mint
+     * @return true if successful
+     */
+    function mint(address account, uint256 amount) public onlyPauser() whenNotPaused() returns (bool) {
+        require(account != address(0), "ERC20: mint to the zero address");
+        _beforeTokenTransfer(address(0), account, amount);
+        _mint(account, amount);
+        return true;
+    }
+
+    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
+     * the total supply.
+     *
+     * Includes a vesting period before address is allowed to use tokens
+     *
+     * Emits a {Transfer} event with `from` set to the zero address.
+     *
+     * Requirements
+     *
+     * - `to` cannot be the zero address.
+     *
+     * @param account The account which tokens will be assigned to
+     * @param amount The amount of tokens to be assigned
+     * @param vestingTime The length of the vesting period (in seconds)
+     */
+    function mintVesting(address account, uint256 amount, uint256 vestingTime) public onlyPauser() whenNotPaused() {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _beforeTokenTransfer(address(0), account, amount);
+        // Should the totalsupply be updated here to avoid cap errors on successful vesting?
+        VestingTokens memory vestingTokens = VestingTokens(account, amount, block.timestamp+vestingTime);
+        _vestingBalances[account] = vestingTokens;
+        emit NewVesting(account, amount, vestingTime);
+    }
+
+    /**
+      * @notice Revokes the amount vested to an account
+      * @param account The account for which vesting is to be revoked
+      *
+     */
+     function revokeVesting(address account) public onlyPauser() whenNotPaused() {
+         require(account != address(0), 'Address cannot be zero');
+         VestingTokens memory vestingTokens = _vestingBalances[account];
+         uint256 revokedAmount = vestingTokens.amount;
+         uint256 revokedDeadline = vestingTokens.deadline;
+         delete _vestingBalances[account];
+         emit RevokeVesting(account, revokedAmount, revokedDeadline);
+     }
+
+    /**
+     *  @notice Checks if account has a vesting schedule
+     *  @param account The account being checked
+     * 
+     */
+     function isVested(address account) public returns (bool) {
+         require(_vestingBalances[account].amount > 0);
+         return true;
+     }
+
+    /**
+     *  @notice Withdrawl of tokens upon completion of vesting period
+     *
+     *
+     */
+     function withdrawVested() public returns (bool) {
+        VestingTokens storage vestingTokens = _vestingBalances[msg.sender];
+        _beforeTokenTransfer(address(0), vestingTokens.beneficiary, vestingTokens.amount);
+        require(vestingTokens.deadline <= block.timestamp, 'Vesting deadline has not passed');
+        uint256 claimedAmount = vestingTokens.amount;
+        _mint(vestingTokens.beneficiary, claimedAmount);
+        vestingTokens.amount = 0;
+        _vestingBalances[msg.sender] = vestingTokens;
+        emit VestingClaimed(vestingTokens.beneficiary, claimedAmount);
+        return true;
+     }
+
+    /**
+     * @dev See {ERC20-_beforeTokenTransfer}.
+     *
+     * Requirements:
+     *
+     * - minted tokens must not cause the total supply to go over the cap.
+     */
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal {
+        if (from == address(0)) { // When minting tokens
+            require(totalSupply().add(amount) <= _cap, "ERC20Capped: cap exceeded");
+        }
+    }
+}
---
+++ test/governance/ATMTokenTest.js
@@ -0,0 +1,144 @@
+// JS Libraries
+const withData = require('leche').withData;
+const { t  } = require('../utils/consts');
+
+// Smart contracts
+const ATMTestToken = artifacts.require("./ATMTestToken.sol");
+
+contract('ATMTokenTest', function (accounts) {
+    let instance;
+    const daoAgent = accounts[0];
+    const daoMember1 = accounts[2];
+    const daoMember2 = accounts[3];
+
+    beforeEach('Setup for each test', async () => {
+        instance = await ATMTestToken.new(10000);
+    });
+
+    withData({
+        _1_mint_no_vesting_basic: [daoMember1, 2000, undefined, false],
+        _2_mint_no_vesting_above_cap: [daoMember1, 11000, 'ERC20Capped: cap exceeded', true]
+    },function(
+        receipent,
+        amount,
+        expectedErrorMessage,
+        mustFail
+    ) {
+        it(t('agent', 'mint', 'Should or should not be able to mint correctly', mustFail), async function() {
+
+            try {
+                // Invocation
+                const result = await instance.mint(receipent, amount, { from: daoAgent });
+                // Assertions
+                assert(!mustFail, 'It should have failed because the amount is greater than the cap');
+                assert(result);
+            } catch (error) {
+                // Assertions
+                assert(mustFail);
+                assert(error);
+                assert.equal(
+                    error.reason,
+                    expectedErrorMessage
+                    );
+            }
+
+        });
+    });
+
+    withData({
+        _3_mint_vesting_basic: [daoMember2, 1000, 7000, undefined, false],
+        _4_mint_vesting_above_cap: [daoMember2, 21000, 7000, 'ERC20Capped: cap exceeded', true]
+    },function(
+        receipent,
+        amount,
+        vestingPeriod,
+        expectedErrorMessage,
+        mustFail
+    ) {
+        it(t('agent', 'mint', 'Should or should not be able to mint correctly', mustFail), async function() {
+
+            try {
+                // Invocation
+                await instance.mintVesting(receipent, amount, vestingPeriod, { from: daoAgent });
+                const result = await instance.isVested(receipent);
+                // Assertions
+                assert(!mustFail, 'It should have failed because the amount is greater than the cap');
+                assert(result);
+            } catch (error) {
+                // Assertions
+                assert(mustFail);
+                assert(error);
+                assert.equal(
+                    error.reason,
+                    expectedErrorMessage
+                    );
+            }
+
+        });
+    });
+
+    withData({
+        _5_set_supply_cap_basic: [70000, daoAgent, undefined, false],
+        _6_set_supply_cap_invalid_sender: [100000, daoMember1, 'PauserRole: caller does not have the Pauser role', true]
+    },function(
+        newCap,
+        sender,
+        expectedErrorMessage,
+        mustFail
+    ) {
+        it(t('agent', 'mint', 'Should or should not be able to mint correctly', mustFail), async function() {
+
+            try {
+                // Invocation
+                await instance.setCap(newCap, { from: sender });
+                const result = await instance.cap();
+                // Assertions
+                assert(!mustFail, 'It should have failed because the sender is invalid');
+                assert(result);
+            } catch (error) {
+                // Assertions
+                assert(mustFail);
+                assert(error);
+                assert.equal(
+                    error.reason,
+                    expectedErrorMessage
+                    );
+            }
+
+        });
+    });
+
+    withData({
+        _7_claim_vested_basic: [daoMember2, 1000, 7000, undefined, false],
+        _8_claim_vested_before_deadline: [daoMember2, 21000, 7000, 'ERC20Capped: cap exceeded', true]
+    },function(
+        receipent,
+        amount,
+        vestingPeriod,
+        expectedErrorMessage,
+        mustFail
+    ) {
+        it(t('agent', 'mint', 'Should or should not be able to mint correctly', mustFail), async function() {
+
+            try {
+                // Invocation
+                await instance.mintVesting(receipent, amount, vestingPeriod, { from: daoAgent });
+                
+                result = await instance.withdrawVested({ from: receipent });
+                // Assertions
+                assert(!mustFail, 'It should have failed because the amount is greater than the cap');
+                assert(result);
+            } catch (error) {
+                // Assertions
+                assert(mustFail);
+                assert(error);
+                assert.equal(
+                    error.reason,
+                    expectedErrorMessage
+                    );
+            }
+
+        });
+    });
+
+})
