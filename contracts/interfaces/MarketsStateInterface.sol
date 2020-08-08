9a5396a3abaaf97048828f0e2cf331ffe6d5506a
Guillermo Daniel Salazar <guillesalazar@gmail.com>
2020-08-08 10:39:53 -0300

    Adding docs and formatting.

--- contracts/interfaces/MarketsStateInterface.sol
+++ contracts/interfaces/MarketsStateInterface.sol
@@ -6,40 +6,77 @@ import "../util/MarketStateLib.sol";
 
 
 /**
-    @notice 
+    @notice This interface defines the function to store and get data from different markets.
 
     @author develop@teller.finance
  */
 interface MarketsStateInterface {
+    /**
+        @notice It increases the repayment amount for a given market.
+        @param borrowedAsset borrowed asset address.
+        @param collateralAsset collateral asset address.
+        @param amount amount to add.
+     */
     function increaseRepayment(
         address borrowedAsset,
         address collateralAsset,
         uint256 amount
     ) external;
 
+    /**
+        @notice It increases the supply amount for a given market.
+        @param borrowedAsset borrowed asset address.
+        @param collateralAsset collateral asset address.
+        @param amount amount to add.
+     */
     function increaseSupply(
         address borrowedAsset,
         address collateralAsset,
         uint256 amount
     ) external;
 
+    /**
+        @notice It increases the borrowed amount for a given market.
+        @param borrowedAsset borrowed asset address.
+        @param collateralAsset collateral asset address.
+        @param amount amount to add.
+     */
     function increaseBorrow(
         address borrowedAsset,
         address collateralAsset,
         uint256 amount
     ) external;
 
+    /**
+        @notice It gets the current supply-to-debt (StD) ratio for a given market.
+        @param borrowedAsset borrowed asset address.
+        @param collateralAsset collateral asset address.
+        @return the supply-to-debt ratio value.
+     */
     function getSupplyToDebt(address borrowedAsset, address collateralAsset)
         external
         view
         returns (uint256);
 
+    /**
+        @notice It gets the supply-to-debt (StD) ratio for a given market, including a new loan amount.
+        @param borrowedAsset borrowed asset address.
+        @param collateralAsset collateral asset address.
+        @param loanAmount a new loan amount to consider in the ratio.
+        @return the supply-to-debt ratio value.
+     */
     function getSupplyToDebtFor(
         address borrowedAsset,
         address collateralAsset,
         uint256 loanAmount
     ) external view returns (uint256);
 
+    /**
+        @notice It gets the current market state.
+        @param borrowedAsset borrowed asset address.
+        @param collateralAsset collateral asset address.
+        @return the current market state.
+     */
     function getMarket(address borrowedAsset, address collateralAsset)
         external
         view
