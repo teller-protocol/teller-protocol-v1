6b723d1912a4b706a27224c94facd32918bd4fd8
Guillermo Daniel Salazar <guillesalazar@gmail.com>
2020-08-14 15:02:43 -0300

    Fixing unit tests.

--- test/base/LendingPoolInitializeTest.js
+++ test/base/LendingPoolInitializeTest.js
@@ -18,17 +18,17 @@ contract('LendingPoolInitializeTest', function (accounts) {
     const getInstance = (refs, index, accountIndex) => index === -1 ? NULL_ADDRESS: index === 99 ? accounts[accountIndex] : refs[index];
 
     withData({
-        _1_basic: [2, 3, 4, 5, 6, 7, 8, undefined, false],
-        _2_notTdai: [-1, 3, 4, 5, 6, 7, 8, 'TTOKEN_ADDRESS_IS_REQUIRED', true],
-        _3_notDai: [2, -1, 4, 5, 6, 7, 8, 'TOKEN_ADDRESS_IS_REQUIRED', true],
-        _4_notLenderInfo: [2, 3, -1, 5, 6, 7, 8, 'LENDERS_ADDRESS_IS_REQUIRED', true],
-        _5_notLoanInfo: [2, 3, 4, -1, 6, 7, 8, 'LOANS_ADDRESS_IS_REQUIRED', true],
-        _6_notCToken: [2, 3, 4, 5, -1, 7, 8, undefined, false],
-        _7_notTdai_notLoanInfo: [-1, 3, 4, -1, 6, 7, 8, 'TTOKEN_ADDRESS_IS_REQUIRED', true],
-        _8_notDai_notLenderInfo: [2, -1, -1, 5, 6, 7, 8, 'TOKEN_ADDRESS_IS_REQUIRED', true],
-        _9_notSettings: [2, 3, 4, 5, 6, -1, 8, 'SETTINGS_MUST_BE_PROVIDED', true],
-        _10_notMarkets: [2, 3, 4, 5, 6, 8, -1, 'MARKETS_MUST_BE_PROVIDED', true],
-        _11_notMarkets_not_contract: [2, 3, 4, 5, 6, 8, 99, 'MARKETS_MUST_BE_A_CONTRACT', true],
+        _1_basic: [2, 3, 4, 5, 6, 7, 8, 9, undefined, false],
+        _2_notTdai: [-1, 3, 4, 5, 6, 7, 8, 9, 'TTOKEN_ADDRESS_IS_REQUIRED', true],
+        _3_notDai: [2, -1, 4, 5, 6, 7, 8, 9, 'TOKEN_ADDRESS_IS_REQUIRED', true],
+        _4_notLenderInfo: [2, 3, -1, 5, 6, 7, 8, 9, 'LENDERS_ADDRESS_IS_REQUIRED', true],
+        _5_notLoanInfo: [2, 3, 4, -1, 6, 7, 8, 9, 'LOANS_ADDRESS_IS_REQUIRED', true],
+        _6_notCToken: [2, 3, 4, 5, -1, 7, 8, 9, undefined, false],
+        _7_notTdai_notLoanInfo: [-1, 3, 4, -1, 6, 7, 8, 9, 'TTOKEN_ADDRESS_IS_REQUIRED', true],
+        _8_notDai_notLenderInfo: [2, -1, -1, 5, 6, 7, 8, 9, 'TOKEN_ADDRESS_IS_REQUIRED', true],
+        _9_notSettings: [2, 3, 4, 5, 6, -1, 8, 9, 'SETTINGS_MUST_BE_PROVIDED', true],
+        _10_notMarkets: [2, 3, 4, 5, 6, 7, -1, 9, 'MARKETS_MUST_BE_PROVIDED', true],
+        _11_notMarkets_not_contract: [2, 3, 4, 5, 6, 7, 99, 9, 'MARKETS_MUST_BE_A_CONTRACT', true],
         _12_notInterestValidator: [2, 3, 4, 5, 6, 7, 8, -1, undefined, false],
         _13_notInterestValidator_not_contract: [2, 3, 4, 5, 6, 7, 8, 99, 'VAL_MUST_BE_EMPTY_OR_CONTRACT', true],
         _14_notInterestValidator_not_contract_2: [2, 3, 4, 5, 6, 7, 8, 9, undefined, false],
@@ -53,8 +53,8 @@ contract('LendingPoolInitializeTest', function (accounts) {
             const loanInfoAddress = getInstance(mocks, loanInfoIndex, 5)
             const cTokenAddress = getInstance(mocks, cTokenIndex, 6);
             const settingsAddress = getInstance(mocks, settingsIndex, 7);
-            const marketsAddress = getInstance(mocks, marketsIndex, 7);
-            const interestValidatorAddress = getInstance(mocks, interestValidatorIndex, 8);
+            const marketsAddress = getInstance(mocks, marketsIndex, 8);
+            const interestValidatorAddress = getInstance(mocks, interestValidatorIndex, 9);
 
             try {
                 // Invocation
@@ -67,7 +67,7 @@ contract('LendingPoolInitializeTest', function (accounts) {
                     settingsAddress,
                     marketsAddress,
                     interestValidatorAddress,
-                );;
+                );
                 
                 // Assertions
                 assert(!mustFail, 'It should have failed because data is invalid.');
