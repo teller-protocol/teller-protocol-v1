cd660d26216f5ef87b619d93b3e8f11ebf8c31ab
Juan Castellon <juan@wearoft.com>
2020-08-14 14:25:30 +0200

    ATMGovernanceFactory dependencies changed [https://teller-finance.atlassian.net/browse/PROTOCOL-157]

--- contracts/ATM/ATMGovernanceFactory.sol
+++ contracts/ATM/ATMGovernanceFactory.sol
@@ -1,14 +1,14 @@
 pragma solidity 0.5.17;
 
 // External Libraries
-import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/utils/Address.sol";
 
 // Common
 import "./ATMGovernance.sol";
 import "../util/AddressLib.sol";
-
+import "../base/TInitializable.sol";
 // Contracts
-import "@openzeppelin/contracts/access/roles/SignerRole.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/access/roles/SignerRole.sol";
 
 // Interfaces
 import "./IATMGovernance.sol";
@@ -18,21 +18,29 @@ import "../settings/ATMSettingsInterface.sol";
     @notice This contract will create upgradeable ATM instances.
     @author develop@teller.finance
  */
-contract ATMGovernanceFactory is SignerRole {
+contract ATMGovernanceFactory is SignerRole, TInitializable {
 
     // Map of ATM instances
     // ATMProxy address => isATM
-    mapping (address => bool) public atmProxies;
+    mapping (address => bool) public atms;
 
     // List of ATM instances
-    address[] public atmProxiesList;
+    address[] public atmList;
 
-    // List of ATM Token instances
-    // ATMToken address => ATM address
-    mapping (address => address) public atmTokens;
-
-
+    
     ATMSettingsInterface private settings;
 
+    function createATM()
+        external
+        onlySigner()
+    {
+        // Deploy ATM base contract
+        // Create new ATM proxy
+        // Set ATM v1
+    }
 
+
+    function initialize(address _settings) public initializer {
+        setting = _settings;
+    }
 } 
