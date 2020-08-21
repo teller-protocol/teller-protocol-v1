f4c54d3e94f2861fe654c117c1e6a61564ed4439
Juan Castellon <juan@wearoft.com>
2020-08-21 14:08:21 +0200

    Comments added to Uniswap interface [https://teller-finance.atlassian.net/browse/PROTOCOL-194]

--- contracts/base/Escrow/Dapps/IUniswap.sol
+++ contracts/base/Escrow/Dapps/IUniswap.sol
@@ -2,6 +2,15 @@ pragma solidity 0.5.17;
 
 interface IUniswap {
 
+    /**
+        @notice Event emmitted every time a successful swap has taken place. 
+        @param from address where source element will be deducted.
+        @param to destination address where elements will be sent to.
+        @param sourceElement source element (ETH or Token) address.
+        @param receivedElement received element (ETH or Token) address.
+        @param sourceAmount element (ETH or Token) amount sent.
+        @param receivedAmount element (ETH or Token) amount received.
+     */
     event UniswapSwapped(
         address indexed from,
         address indexed to,
