40d8f5617d0138f57f8def942d0dc46865c227eb
Noah Passalacqua <noah.passa@gmail.com>
2020-08-28 13:46:45 -0500

    Merge inverse and normal aggregator tests.

--- contracts/providers/chainlink/ChainlinkPairAggregatorProxy.sol
+++ contracts/providers/chainlink/ChainlinkPairAggregatorProxy.sol
@@ -8,7 +8,6 @@ contract ChainlinkPairAggregatorProxy is BaseProxy, BaseChainlinkPairAggregator 
     /**
         @notice It is a Proxy contract for ChainlinkPairAggregators that uses the logic implementation as defined in the global Settings contract.
         @param settingsAddress the global Settings address.
-        @param isInverse determines whether or not this pair aggregator is inverse.
     */
     constructor(address settingsAddress)
         public
--- test/providers/chainlink/ChainlinkPairAggregatorGetPreviousAnswerTest.js
+++ test/providers/chainlink/ChainlinkPairAggregatorGetPreviousAnswerTest.js
@@ -1,5 +1,6 @@
 // JS Libraries
 const withData = require('leche').withData;
+const BigNumber = require('bignumber.js');
 const { t } = require('../../utils/consts');
 const AggregatorInterfaceEncoder = require('../../utils/encoders/AggregatorInterfaceEncoder');
 
@@ -12,32 +13,40 @@ const ChainlinkPairAggregator = artifacts.require("./providers/chainlink/Chainli
 contract('ChainlinkPairAggregatorGetPreviousAnswerTest', function (accounts) {
     const aggregatorInterfaceEncoder = new AggregatorInterfaceEncoder(web3);
     let chainlinkAggregator;
-    let instance;
-    
+
     beforeEach('Setup for each test', async () => {
         chainlinkAggregator = await ChainlinkAggregatorMock.new();
-        assert(chainlinkAggregator);
-        assert(chainlinkAggregator.address);
-
-        instance = await ChainlinkPairAggregator.new();
-        instance.initialize(
-            chainlinkAggregator.address,
-            false,
-            18,
-            18
-        );
-        assert(instance);
-        assert(instance.address);
     });
 
+    const getExpectedAnswer = (value, collateralDecimals, responseDecimals, isInverse) => {
+        value = new BigNumber(value);
+        if (isInverse) {
+            const wholeCollateral = new BigNumber(10).pow(collateralDecimals);
+            const wholeResponse = new BigNumber(10).pow(responseDecimals);
+            return wholeCollateral.times(wholeResponse).div(value);
+        } else {
+            if (collateralDecimals >= responseDecimals) {
+                return value.times(new BigNumber(10).pow(new BigNumber(collateralDecimals).minus(responseDecimals)));
+            } else {
+                return value.div(new BigNumber(10).pow(new BigNumber(responseDecimals).minus(collateralDecimals)));
+            }
+        }
+    };
+
     withData({
-        _1_basic: [5, 120, 3, undefined, false],
-        _2_zeroDays: [6, 120, 0, undefined, false],
-        _3_notHistory: [5, 120, 8, 'NOT_ENOUGH_HISTORY', true]
+        _1_basic: [3, 5, false, 18, 18, 120, undefined, false],
+        _2_zero_days: [0, 6, false, 18, 18, 120, undefined, false],
+        _3_not_history: [8, 5, false, 18, 18, 120, 'NOT_ENOUGH_HISTORY', true],
+        // LINK => 18 decimals, oracle response => 8 decimals, 1 USD = 4.033 (or 403300000 -8 decimals-)
+        _4_link_usd: [2, 3, true, 8, 18, "403300000", undefined, false],
+        _5_usd_tokenC: [0, 3, true, 10, 10, "120030405060", undefined, false],
     }, function(
+        roundsBack,
         latestRoundResponse,
-        getAnswerResponse,
-        backTimestamp,
+        isInverse,
+        responseDecimals,
+        collateralDecimals,
+        latestAnswerResponse,
         expectedErrorMessage,
         mustFail,
     ) {    
@@ -47,14 +56,24 @@ contract('ChainlinkPairAggregatorGetPreviousAnswerTest', function (accounts) {
             await chainlinkAggregator.givenMethodReturnUint(latestRoundEncodeAbi, latestRoundResponse.toString());
 
             const getAnswerEncodeAbi = aggregatorInterfaceEncoder.encodeGetAnswer();
-            await chainlinkAggregator.givenMethodReturnUint(getAnswerEncodeAbi, getAnswerResponse.toString());
+            await chainlinkAggregator.givenMethodReturnUint(getAnswerEncodeAbi, latestAnswerResponse.toString());
+
+            const expectedLatestAnswer = getExpectedAnswer(latestAnswerResponse, collateralDecimals, responseDecimals, isInverse);
+            const instance = await ChainlinkPairAggregator.new();
+            await instance.initialize(
+                chainlinkAggregator.address,
+                isInverse,
+                responseDecimals,
+                collateralDecimals
+            );
 
             try {
                 // Invocation
-                const result = await instance.getPreviousAnswer(backTimestamp);
+                const result = await instance.getPreviousAnswer(roundsBack);
                 
                 // Assertions
                 assert(!mustFail, 'It should have failed because data is invalid.');
+                assert.equal(result.toString(), expectedLatestAnswer.toString());
                 assert(result);
             } catch (error) {
                 // Assertions
