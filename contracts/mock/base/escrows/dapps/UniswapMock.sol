4946953d7e1b0d792d825d4db73b19407aba706b
Guillermo Daniel Salazar <guillesalazar@gmail.com>
2020-09-03 03:06:23 -0300

    Moving to escrows/dapps.

--- contracts/mock/base/escrows/dapps/UniswapMock.sol
+++ contracts/mock/base/escrows/dapps/UniswapMock.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.5.17;
 
-import "../../../../base/Escrow/Dapps/Uniswap.sol";
+import "../../../../base/escrows/dapps/Uniswap.sol";
 
 /**
     @notice This mock is used to expose a payable fallback function on tests.f
