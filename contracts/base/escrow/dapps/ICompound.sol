b1d1a097a39dac99c1b22e0702ab6417d30500c6
Noah Passalacqua <noah.passa@gmail.com>
2020-09-07 17:16:23 -0500

    Put functions back in Dapp interfaces.

--- contracts/base/escrow/dapps/ICompound.sol
+++ contracts/base/escrow/dapps/ICompound.sol
@@ -7,6 +7,26 @@ pragma solidity 0.5.17;
  */
 interface ICompound {
     /**
+        @notice To lend we first have to approve the cToken to access the token balance then mint. 
+        @param cTokenAddress address of the token.
+        @param amount amount of tokens to mint. 
+    */
+    function lend(address cTokenAddress, uint256 amount) external;
+
+    /**
+        @notice This function calls Compound redeemUnderlying().
+        @param cTokenAddress address of the token.
+        @param amount amount of underlying tokens to redeem.
+    */
+    function redeem(address cTokenAddress, uint256 amount) external;
+
+    /**
+        @notice This function redeems complete token balance.
+        @param cTokenAddress address of the token.
+    */
+    function redeemAll(address cTokenAddress) external;
+
+    /**
         @notice This event is emitted every time Compound lend is invoked successfully.
         @param sender the sender of this transaction.
         @param dappAddress address of the compound app.
--- contracts/base/escrow/dapps/IUniswap.sol
+++ contracts/base/escrow/dapps/IUniswap.sol
@@ -6,6 +6,26 @@ pragma solidity 0.5.17;
  */
 interface IUniswap {
     /**
+        @notice Swaps ETH/Tokens for Tokens/ETH using the Uniswap protocol.
+        @param canonicalWeth address of the canonical WETH in the current network.
+        @param routerAddress address of the Uniswap Router.
+        @param path An array of token addresses. path.length must be >= 2. Pools for each consecutive pair of addresses must exist and have liquidity.
+        @param sourceAmount amount of source element (ETH or Tokens) to swap.
+        @param minDestination The minimum amount of output tokens that must be received for the transaction not to revert.
+        @dev This function mainly invokes 3 Uniswap external functions:
+            https://uniswap.org/docs/v2/smart-contracts/router02/#swapexactethfortokens
+            https://uniswap.org/docs/v2/smart-contracts/router02/#swapexacttokensforeth
+            https://uniswap.org/docs/v2/smart-contracts/router02/#swapexacttokensfortokens
+     */
+    function swap(
+        address canonicalWeth,
+        address routerAddress,
+        address[] calldata path,
+        uint256 sourceAmount,
+        uint256 minDestination
+    ) external;
+
+    /**
         @notice Event emmitted every time a successful swap has taken place. 
         @param from address where source element will be deducted.
         @param to destination address where elements will be sent to.
