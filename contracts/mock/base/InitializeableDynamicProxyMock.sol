6024272276b17d880896ade71ec86ef2fbb1c375
Guillermo Daniel Salazar <guillesalazar@gmail.com>
2020-09-10 13:12:47 -0300

    Formatting contracts.

--- contracts/mock/base/InitializeableDynamicProxyMock.sol
+++ contracts/mock/base/InitializeableDynamicProxyMock.sol
@@ -3,7 +3,6 @@ pragma solidity 0.5.17;
 import "../../base/InitializeableDynamicProxy.sol";
 
 contract InitializeableDynamicProxyMock is InitializeableDynamicProxy {
-
     function externalLogicName() external view returns (bytes32 name) {
         return logicName();
     }
