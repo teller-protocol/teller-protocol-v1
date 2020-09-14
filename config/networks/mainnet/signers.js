d33404171a6f7bfe25736d8c69da1c70ffa49cfd
Noah Passalacqua <noah@passabilities.dev>
2020-09-14 13:54:05 -0500

    Rename network config files to be JSON.

--- config/networks/mainnet/signers.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    _1: '0x54272B8AB4c977A480C032666765aACb4bDA9D83',
-};
--- config/networks/soliditycoverage/index.js
+++ config/networks/soliditycoverage/index.js
@@ -1,12 +1,12 @@
 module.exports = {
     network: 'soliditycoverage',
-    chainlink: require('./chainlink'),
-    compound: require('./compound'),
-    tokens: require('./tokens'),
-    assetSettings: require('./assetSettings'),
-    platformSettings: require('./platformSettings'),
-    signers: require('./signers'),
-    atms: require('./atms'),
+    chainlink: require('./chainlink.json'),
+    compound: require('./compound.json'),
+    tokens: require('./tokens.json'),
+    assetSettings: require('./assetSettings.json'),
+    platformSettings: require('./platformSettings.json'),
+    signers: require('./signers.json'),
+    atms: require('./atms.json'),
     maxGasLimit: 15500000,
     toTxUrl: ({ tx }) => {
         return `not-supported-url`;
