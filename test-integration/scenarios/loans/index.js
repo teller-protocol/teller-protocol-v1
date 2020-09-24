0a69e595300ba71428bbf8c36f728f196edd29a7
Guillermo Daniel Salazar <guillesalazar@gmail.com>
2020-09-24 18:32:57 -0300

    Improving integration tests.

---
+++ test-integration/scenarios/loans/index.js
@@ -0,0 +1,5 @@
+const _1_takeout_loan_and_2_repays_full_successfully = require('./1_takeout_loan_and_2_repays_full_successfully');
+
+module.exports = {
+    _1_takeout_loan_and_2_repays_full_successfully: _1_takeout_loan_and_2_repays_full_successfully,
+};
