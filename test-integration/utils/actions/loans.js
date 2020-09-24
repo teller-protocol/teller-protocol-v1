ac8078597a117acd13a9ed210605e0e400726606
Guillermo Daniel Salazar <guillesalazar@gmail.com>
2020-09-24 17:36:13 -0300

    Adding new actions.

--- test-integration/utils/actions/loans.js
+++ test-integration/utils/actions/loans.js
@@ -8,18 +8,18 @@ const {
 } = require("../../../test/utils/consts");
 const {
   createMultipleSignedLoanTermsResponses,
-  createLoanTermsRequest
+  createLoanTermsRequest,
 } = require("../../../test/utils/loan-terms-helper");
 const {printFullLoan} = require("../../../test/utils/printer");
 const {
   lendingPool: lendingPoolEvents,
-  loans: loansEvents
+  loans: loansEvents,
 } = require("../../../test/utils/events");
 
 const depositFunds = async (
-  { token, lendingPool },
-  { txConfig, testContext },
-  { amount }
+  {token, lendingPool},
+  {txConfig, testContext},
+  {amount}
 ) => {
   console.log("Depositing funds on pool...");
   await token.approve(lendingPool.address, amount, txConfig);
@@ -36,20 +36,20 @@ const requestLoanTerms = async (
   {loanTermsRequestTemplate, loanResponseTemplate}
 ) => {
   console.log("Requesting loan terms...");
-  const { web3, timer, nonces, chainId } = testContext;
+  const {web3, timer, nonces, chainId} = testContext;
   const currentTimestamp = parseInt(await timer.getCurrentTimestamp());
   const {
     amount,
     durationInDays,
     borrower,
-    recipient = NULL_ADDRESS
+    recipient = NULL_ADDRESS,
   } = loanTermsRequestTemplate;
   const {
     interestRate,
     collateralRatio,
     maxLoanAmount,
     signers,
-    responseTime
+    responseTime,
   } = loanResponseTemplate;
 
   const loanTermsRequestInfo = {
@@ -165,14 +165,14 @@ const withdrawCollateral = async (
 };
 
 const takeOutLoan = async (
-  { loans },
-  { txConfig, testContext },
-  { loanId, amount }
+  {loans},
+  {txConfig, testContext},
+  {loanId, amount}
 ) => {
   console.log(`Taking out loan id ${loanId}...`);
   const takeOutLoanResult = await loans.takeOutLoan(loanId, amount, txConfig);
   const loanInfo = await loans.loans(loanId);
-  const { escrow } = loanInfo;
+  const {escrow} = loanInfo;
   loansEvents
     .loanTakenOut(takeOutLoanResult)
     .emitted(loanId, txConfig.from, escrow, amount);
@@ -205,14 +205,14 @@ const liquidateLoan = async (
   {loanId, amount}
 ) => {
   console.log(`Liquidating loan id ${loanId}...`);
-  const { web3 } = testContext;
+  const {web3} = testContext;
 
   const initialTotalCollateral = await loans.totalCollateral();
   const liquidateEthPrice = await settings.getPlatformSettingValue(
     toBytes32(web3, platformSettingsNames.LiquidateEthPrice)
   );
   const getCollateralInfo = await loans.getCollateralInfo(loanId);
-  const { neededInLendingTokens } = getCollateralInfo;
+  const {neededInLendingTokens} = getCollateralInfo;
   const transferAmountToLiquidate = BigNumber(neededInLendingTokens.toString())
     .times(liquidateEthPrice)
     .div(10000);
