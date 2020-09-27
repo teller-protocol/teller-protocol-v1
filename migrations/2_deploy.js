0daac40f0a304ed6ea3ce5247556ce6402739f86
Juan Castellon <juan@wearoft.com>
2020-09-27 19:31:45 +0200

    Liquidity mining withdrawTRL unit tests and modifications [PROTOCOL-184]

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -231,9 +231,10 @@ module.exports = async function(deployer, network, accounts) {
   deployerApp.writeJson();
   console.log(`${'='.repeat(25)} Deployment process finished. ${'='.repeat(25)}`);
   } catch (error) {
-    console.log(error);
+    console.log('\x1b[33m\x1b[41m\x1b[5m', `Error deploying contract`, '\x1b[0m');
+    console.log(error);    
     deployerApp.print();
     deployerApp.writeJson();
-    console.log(`${'='.repeat(25)} Deployment process FAILED. ${'='.repeat(25)}`);
+    console.log('\x1b[33m\x1b[41m\x1b[5m%s\x1b[0m', `${'='.repeat(25)} Deployment process FAILED. ${'='.repeat(25)}`);
   }
 };
