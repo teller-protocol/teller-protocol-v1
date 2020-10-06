11aff94ce3c5aa0184c6d5868dbfb1dc225df0d5
Juan Castellon <juan@wearoft.com>
2020-10-06 21:18:29 +0200

    Liquidity mining pauser role and blacklist addresses added [PROTOCOL-184]

--- test/atm/ATMLiquidityMiningCalculateAccruedTLRTest.js
+++ test/atm/ATMLiquidityMiningCalculateAccruedTLRTest.js
@@ -12,18 +12,32 @@ const TLRToken = artifacts.require("./atm/TLRToken.sol");
 const ATMGovernance = artifacts.require("./atm/ATMGovernance.sol");
 const ATMLiquidityMining = artifacts.require("./atm/ATMLiquidityMining.sol");
 const TDAI = artifacts.require("./base/TDAI.sol");
+const IATMSettingsEncoder = require('../utils/encoders/IATMSettingsEncoder');
+const SettingsInterfaceEncoder = require('../utils/encoders/SettingsInterfaceEncoder');
 
 contract("ATMLiquidityMiningCalculateAccruedTLRTest", function(accounts) {
+    const atmSettingsEncoder = new IATMSettingsEncoder(web3);
+    const settingsInterfaceEncoder = new SettingsInterfaceEncoder(web3);
     const owner = accounts[0]; 
     const user = accounts[2];
     const INITIAL_REWARD = 1;
     const MAX_BLOCK_INTERVAL = 10;
+    const FLOATING_SHOULD_RESET_AFTER_STAKE_OPERATION = 0;
     let instance;
     let governance;
     let tToken;
 
     beforeEach("Setup for each test", async () => {
         const settings = await Mock.new();
+        const atmSettingsInstance = await Mock.new();
+        await atmSettingsInstance.givenMethodReturnBool(
+            atmSettingsEncoder.encodeIsATMPaused(),
+            false
+        );
+        await settings.givenMethodReturnAddress(
+            settingsInterfaceEncoder.encodeATMSettings(),
+            atmSettingsInstance.address
+        );
         governance = await ATMGovernance.new();
         await governance.initialize(settings.address, owner, INITIAL_REWARD);
         const tlr = await TLRToken.new();   
@@ -31,6 +45,7 @@ contract("ATMLiquidityMiningCalculateAccruedTLRTest", function(accounts) {
         instance = await ATMLiquidityMining.new();
         await instance.initialize(settings.address, governance.address, tlr.address, { from: owner });
     });
+
     withData({
         _1_no_stake_only_initial_reward: [ [], 0, false, undefined ],
         _2_no_stake_: [ [3, 6, 12], 0, false, undefined ],
@@ -59,8 +74,9 @@ contract("ATMLiquidityMiningCalculateAccruedTLRTest", function(accounts) {
             }
         });
     });
+
     withData({
-        _1_one_reward_multi_stake: [ [10, 10, 10, 10, 10, 10, 10, 10, 10], [0, 90, 280, 570, 960, 1450, 2040, 2730, 3520], false, undefined ],
+        _1_one_reward_multi_stake: [ [10, 10, 10, 10, 10, 10, 10, 10, 10], [0, 100, 300, 600, 1000, 1500, 2100, 2800, 3600], false, undefined ],
     }, function(amounts,expectedResults, mustFail, expectedErrorMessage) {
         it(t("user", "calculateAccruedTLR#2-one-reward-multiple-stakes", "Should be able or not to stake tTokens.", mustFail), async function() {
             try {
@@ -69,57 +85,57 @@ contract("ATMLiquidityMiningCalculateAccruedTLRTest", function(accounts) {
                     const before = parseInt((await helper.latestBlock()).number);
                     await tToken.mint(user, amounts[i], { from: owner });
                     await tToken.approve(instance.address, amounts[i], { from: user });
+                    const tx = await instance.stake(tToken.address, amounts[i] , { from: user });
                     // Invocation 
                     const result = parseInt(await instance.getTLRTotalBalance.call({from: user}));
                     // Assertions
                     assert(!mustFail, 'It should have failed because data is invalid.');
                     // Validating results
                     assert.equal(result, expectedResults[i], "Error validating accrued TLR");
-                    const tx = await instance.stake(tToken.address, amounts[i] , { from: user });
-                    const interval = parseInt(tx.receipt.blockNumber) - before;
-                    const advance = MAX_BLOCK_INTERVAL - interval;
+                    // Advance blocks
+                    const setupBlocksInterval = parseInt(tx.receipt.blockNumber) - before;
+                    const advance = MAX_BLOCK_INTERVAL - setupBlocksInterval;
                     await helper.advanceBlocks(advance);
                 }
             } catch (error) {
-                console.log(error);
                 assert(mustFail);
                 assert(error);
                 assert.equal(error.reason, expectedErrorMessage);
             }
         });
     });
- /*
+    
+ 
     withData({
-        _1_multi_reward_multi_stake: [ [10, 100], [20, 200, 2000], false, undefined ],
-    }, function(amounts, rewards, mustFail, expectedErrorMessage) {
+        _1_multi_reward_multi_stake: [ [10, 100], [10, 100, 1000], [100, 10100, 1010100, 1020100], false, undefined ],
+    }, function(amounts, rewards, expectedResults, mustFail, expectedErrorMessage) {
         it(t("user", "calculateAccruedTLR#3-multiple-rewards-multiple-stakes", "Should be able or not to stake tTokens.", mustFail), async function() {
-            // Setup 
-            // Initial stake
-            await tToken.mint(user, amounts[0], { from: owner });
-            await tToken.approve(instance.address, amounts[0], { from: user });
-            await instance.stake(tToken.address, amounts[0] , { from: user });
+            // Setup
             const totalAmounts = amounts.reduce((a, b) => a + b, 0);        
-            // Adding new rewards
-            for (let r = 0; r < rewards.length; r++){
-                await helper.advanceBlocks(rewards[r]);
-                await governance.addTLRReward(rewards[r], { from: owner});
-            }
-            await tToken.mint(user, amounts[1], { from: owner });
+            await tToken.mint(user, totalAmounts, { from: owner });
             await tToken.approve(instance.address, totalAmounts, { from: user });
-            
+            // Initial stake
+            await instance.stake(tToken.address, amounts[0] , { from: user });
             try {
-                // Invocation 
+                // Adding new rewards as owner
+                for (let r = 0; r < rewards.length; r++){
+                    await helper.advanceBlocks(rewards[r] - 1 );
+                    await governance.addTLRReward(rewards[r], { from: owner});
+                    const accrued = parseInt(await instance.getTLRTotalBalance.call({from: user}));
+                    const floating = parseInt(await instance.getTLRFloatingBalance.call({from: user}));
+                    // Validating both accrued and floating balances are equal
+                    assert.equal(accrued, floating, "Both TLR accrued balances should be equal as no other STAKE(), UNSTAKE(), WITHDRAW() operation has been called.");
+                    assert.equal(floating, expectedResults[r], "Incorrect floating TLR calculation.")
+                }
+                // Invocation - second stake 
                 const result = await instance.stake(tToken.address, amounts[1] , { from: user });
                 // Assertions
                 assert(!mustFail, 'It should have failed because data is invalid.');
                 // Validating result
-                const liquidityBalanceAfter = parseInt(await tToken.balanceOf(instance.address)) ;
-                assert.equal(liquidityBalanceAfter, totalAmounts, "Liquidity contract tToken balance error");
+                const floating = parseInt(await instance.getTLRFloatingBalance.call({from: user}));
+                assert.equal(floating, FLOATING_SHOULD_RESET_AFTER_STAKE_OPERATION, "Floating should be zero after stake().");
                 const accruedTLRBalance = parseInt(await instance.getTLRTotalBalance.call({from: user}));
-                // Validating events were emitted
-                liquidityMining
-                    .stake(result)
-                    .emitted(user, tToken.address, amounts[1], result.receipt.blockNumber, totalAmounts, accruedTLRBalance);
+                assert.equal(accruedTLRBalance, expectedResults[expectedResults.length - 1], "Incorrect accrued TLR calculation.")
             } catch (error) {
                 assert(mustFail);
                 assert(error);
@@ -127,6 +143,5 @@ contract("ATMLiquidityMiningCalculateAccruedTLRTest", function(accounts) {
             }
         });
     });
-    */
 
 });
