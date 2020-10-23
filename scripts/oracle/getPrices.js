18c71bcc3ff6b56292894da946146d6acf5945d6
Noah Passalacqua <noah@passabilities.dev>
2020-10-23 17:02:42 -0500

    Update to getPrices script.

--- scripts/oracle/getPrices.js
+++ scripts/oracle/getPrices.js
@@ -5,34 +5,34 @@ const { oracle: readParams } = require("../utils/cli-builder");
 const { tokens, teller } = require("../../scripts/utils/contracts");
 const ProcessArgs = require('../utils/ProcessArgs');
 const { toDecimals, toUnits, ETH_ADDRESS } = require("../../test/utils/consts");
-const { TOKEN_NAME, COLL_TOKEN_NAME, AMOUNT, BACK_ROUNDS } = require("../utils/cli/names");
+const { BASE_TOKEN_NAME, QUOTE_TOKEN_NAME, AMOUNT } = require("../utils/cli/names");
 const processArgs = new ProcessArgs(readParams.getPrices().argv);
 
 module.exports = async (callback) => {
     try {
 
-        const collTokenName = processArgs.getValue(COLL_TOKEN_NAME.name);
-        const tokenName = processArgs.getValue(TOKEN_NAME.name);
+        const baseTokenName = processArgs.getValue(BASE_TOKEN_NAME.name);
+        const quoteTokenName = processArgs.getValue(QUOTE_TOKEN_NAME.name);
         const amount = processArgs.getValue(AMOUNT.name);
 
         const getContracts = processArgs.createGetContracts(artifacts);        
         const chainlinkAggregator = await getContracts.getDeployed(teller.chainlinkAggregator());
-        const sourceToken = tokenName.toUpperCase() === 'ETH' ?  {address: ETH_ADDRESS, decimals: async () => Promise.resolve(18) }: await getContracts.getDeployed(tokens.get(tokenName));
-        const targetToken = collTokenName.toUpperCase() === 'ETH' ?  {address: ETH_ADDRESS, decimals: async () => Promise.resolve(18) }: await getContracts.getDeployed(tokens.get(collTokenName));
+        const baseToken = baseTokenName.toUpperCase() === 'ETH' ?  {address: ETH_ADDRESS, decimals: async () => Promise.resolve(18) }: await getContracts.getDeployed(tokens.get(baseTokenName));
+        const quoteToken = quoteTokenName.toUpperCase() === 'ETH' ?  {address: ETH_ADDRESS, decimals: async () => Promise.resolve(18) }: await getContracts.getDeployed(tokens.get(quoteTokenName));
 
-        const decimals = await sourceToken.decimals()
+        const decimals = await baseToken.decimals()
         const amountWithDecimals = toDecimals(amount, decimals.toString()).toFixed(0);
 
-        const targetTokenDecimals = await targetToken.decimals();
+        const quoteTokenDecimals = await quoteToken.decimals();
         
-        console.log(`Chainlink Aggregator => ${tokenName} / ${collTokenName}: `);
+        console.log(`Chainlink Aggregator => ${baseTokenName} / ${quoteTokenName}: `);
         console.log(`${'-'.repeat(60)}`);
 
-        const getLatestAnswerResult = await chainlinkAggregator.latestAnswerFor(sourceToken.address, targetToken.address);
-        console.log(`Lastest Answer:        1 ${tokenName} = ${getLatestAnswerResult.toString()} = ${toUnits(getLatestAnswerResult.toString(), targetTokenDecimals)} ${collTokenName}`);
+        const getLatestAnswerResult = await chainlinkAggregator.latestAnswerFor(baseToken.address, quoteToken.address);
+        console.log(`Lastest Answer:        1 ${baseTokenName} = ${getLatestAnswerResult.toString()} = ${toUnits(getLatestAnswerResult.toString(), quoteTokenDecimals)} ${quoteTokenName}`);
 
-        const latestValueResult = await chainlinkAggregator.valueFor(sourceToken.address, targetToken.address, amountWithDecimals);
-        console.log(`Lastest Value:         ${amount} ${tokenName} = ${latestValueResult.toString()} = ${toUnits(latestValueResult.toString(), targetTokenDecimals)} ${collTokenName}`);
+        const latestValueResult = await chainlinkAggregator.valueFor(baseToken.address, quoteToken.address, amountWithDecimals);
+        console.log(`Lastest Value:         ${amount} ${baseTokenName} = ${latestValueResult.toString()} = ${toUnits(latestValueResult.toString(), quoteTokenDecimals)} ${quoteTokenName}`);
 
         console.log(`${'-'.repeat(60)}`);
         console.log('>>>> The script finished successfully. <<<<');
