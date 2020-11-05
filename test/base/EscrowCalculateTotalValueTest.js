482f28fc93239fb474b78a813868ff4a80fac24b
Noah Passalacqua <noah@passabilities.dev>
2020-11-04 18:24:42 -0600

    Do not include collateral buffer in Escrow value calculation.

--- test/base/EscrowCalculateTotalValueTest.js
+++ test/base/EscrowCalculateTotalValueTest.js
@@ -21,28 +21,25 @@ contract("EscrowCalculateTotalValueTest", function(accounts) {
   const loansEncoder = new LoansBaseInterfaceEncoder(web3);
 
   let instance;
-  const collateralBuffer = 1500;
 
   beforeEach(async () => {
-    const settings = await createTestSettingsInstance(Settings, { from: accounts[0], Mock }, {
-      [settingsNames.CollateralBuffer]: collateralBuffer
-    });
+    const settings = await createTestSettingsInstance(Settings, { from: accounts[0], Mock });
 
     instance = await Escrow.new();
     await instance.externalSetSettings(settings.address);
   });
 
   withData({
-    _1_1_tokens_with_collateral_ratio_eth: [ [ 1000 ], 100, 20, true, 1085 ],
-    _2_2_tokens_with_collateral_ratio_eth: [ [ 1000, 2000 ], 200, 20, true, 3170 ],
-    _3_3_tokens_with_collateral_ratio_eth: [ [ 1000, 2000, 3000 ], 300, 20, true, 6255 ],
+    _1_1_tokens_with_collateral_ratio_eth: [ [ 1000 ], 100, 20, true, 1100 ],
+    _2_2_tokens_with_collateral_ratio_eth: [ [ 1000, 2000 ], 200, 20, true, 3200 ],
+    _3_3_tokens_with_collateral_ratio_eth: [ [ 1000, 2000, 3000 ], 300, 20, true, 6300 ],
     _4_1_tokens_with_zero_collateral_eth: [ [ 1000 ], 0, 0, true, 1000 ],
     _5_2_tokens_with_zero_collateral_eth: [ [ 1000, 2000 ], 0, 0, true, 3000 ],
     _6_3_tokens_with_zero_collateral_eth: [ [ 1000, 2000, 3000 ], 0, 0, true, 6000 ],
 
-    _7_1_tokens_with_collateral_ratio_token: [ [ 1000 ], 100, 20, false, 1085 ],
-    _8_2_tokens_with_collateral_ratio_token: [ [ 1000, 2000 ], 200, 20, false, 3170 ],
-    _9_3_tokens_with_collateral_ratio_token: [ [ 1000, 2000, 3000 ], 300, 20, false, 6255 ],
+    _7_1_tokens_with_collateral_ratio_token: [ [ 1000 ], 100, 20, false, 1100 ],
+    _8_2_tokens_with_collateral_ratio_token: [ [ 1000, 2000 ], 200, 20, false, 3200 ],
+    _9_3_tokens_with_collateral_ratio_token: [ [ 1000, 2000, 3000 ], 300, 20, false, 6300 ],
     _10_1_tokens_with_zero_collateral_token: [ [ 1000 ], 0, 0, false, 1000 ],
     _11_2_tokens_with_zero_collateral_token: [ [ 1000, 2000 ], 0, 0, false, 3000 ],
     _12_3_tokens_with_zero_collateral_token: [ [ 1000, 2000, 3000 ], 0, 0, false, 6000 ]
@@ -77,14 +74,12 @@ contract("EscrowCalculateTotalValueTest", function(accounts) {
       await instance.mockLoans(loans.address);
 
       for (let i = 0; i < tokensAddresses.length; i++) {
-        await instance.mockValueOfIn(tokensAddresses[i], ETH_ADDRESS, tokenAmounts[i]);
+        const tokenAmount = tokenAmounts[i]
+        await instance.mockValueOfIn(tokensAddresses[i], ETH_ADDRESS, tokenAmount);
       }
 
-      // mock the calculation of the collateral buffer
       if (!collateralIsEth) {
-        const collAmount = new BN(collateralAmount)
-        const collateralMinusBuffer = collAmount.minus(collAmount.multipliedBy(collateralBuffer).dividedBy(10000))
-        instance.mockValueOfIn(collateralAddress, ETH_ADDRESS, collateralMinusBuffer.toFixed());
+        instance.mockValueOfIn(collateralAddress, ETH_ADDRESS, collateralAmount);
       }
 
       const valueInToken = 1234567890;
