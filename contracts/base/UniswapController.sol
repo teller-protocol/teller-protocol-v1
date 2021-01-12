9d53a397eb502129a5794378aef605b2dbb3e46e
Jeremi G <gendronjeremi@gmail.com>
2021-01-12 12:22:57 -0500

    Remove: factory

--- contracts/base/UniswapController.sol
+++ contracts/base/UniswapController.sol
@@ -1,6 +1,5 @@
 pragma solidity 0.5.17;
 
-import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router01.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
@@ -12,8 +11,6 @@ contract UniswapController is Base, IUniswapController {
     using NumbersLib for *;
     using SafeERC20 for IERC20;
 
-    IUniswapV2Factory private factory;
-
     IUniswapV2Router01 private constant ROUTER = IUniswapV2Router01(
         0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D
     );
@@ -23,11 +20,9 @@ contract UniswapController is Base, IUniswapController {
     uint256 private minAmountOutPercent;
     uint256 private maxSecondsToSwap;
 
-    function initialize(
-        address factoryAddress,
-        uint256 _minAmountOutPercent,
-        uint256 _maxSecondsToSwap
-    ) external {
+    function initialize(uint256 _minAmountOutPercent, uint256 _maxSecondsToSwap)
+        external
+    {
         require(
             _minAmountOutPercent >= 8000 && _minAmountOutPercent < 10000,
             "MIN_AMOUNT_INVALID"
@@ -39,7 +34,6 @@ contract UniswapController is Base, IUniswapController {
 
         minAmountOutPercent = _minAmountOutPercent;
         maxSecondsToSwap = _maxSecondsToSwap;
-        factory = IUniswapV2Factory(factoryAddress);
         chainlink = _getSettings().chainlinkAggregator();
     }
 
