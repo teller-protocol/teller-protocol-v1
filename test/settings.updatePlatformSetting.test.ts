ba7c49a704ea234f70ab8f20cbd8142d0d7f708e
sypeer <sy.peeran@gmail.com>
2021-02-11 19:56:06 -0500

    updating settings test

--- test/settings.updatePlatformSetting.test.ts
+++ test/settings.updatePlatformSetting.test.ts
@@ -1,39 +1,51 @@
-import { Contract } from '@ethersproject/contracts';
 import { expect, should } from 'chai';
-import { deployments, ethers } from 'hardhat';
+import { deployments, ethers, contracts, getNamedSigner } from 'hardhat';
+import { Settings } from '../types/typechain';
 
 should();
 
-describe('Update platform setting', () => {
-  const newCollateralBufferValue = 2000;
-  let settingsInstance: Contract;
+describe('Settings', async () => {
+  let settings: Settings
 
-  before(async () => {
-    await deployments.fixture('test');
+  describe('Update platform setting', () => {
+    const newCollateralBufferValue = 2000
 
-    // Load settings instance
-    const settingsAbi = await deployments.get('Settings');
-    settingsInstance = await ethers.getContractAt('Settings', settingsAbi.address);
-  });
+    beforeEach(async () => {
+      await deployments.fixture('settings')
+      console.log('Pass')
+      const deployer = await getNamedSigner('deployer')
+      settings = await contracts.get<Settings>('Settings', { from: deployer })
+    })
 
-  it('Should be able to update a platform setting as a pauser', async () => {
-    // Sender address
-    const pauser = (await ethers.getSigners())[1];
+    it('Should be able to update a platform setting as a pauser', async () => {
 
-    // Update setting
-    await settingsInstance.connect(pauser).updatePlatformSetting(ethers.utils.formatBytes32String('CollateralBuffer'), newCollateralBufferValue);
+      // Update setting
+      await settings
+        .updatePlatformSetting(
+          ethers.utils.formatBytes32String('CollateralBuffer'),
+          newCollateralBufferValue
+        )
 
-    const newCollateralBuffer = (await settingsInstance.getPlatformSetting(ethers.utils.formatBytes32String('CollateralBuffer'))).value;
+      const { value: newCollateralBuffer } = await settings.getPlatformSetting(ethers.utils.formatBytes32String('CollateralBuffer'))
 
-    // Assert setting
-    expect(newCollateralBuffer).to.equal(newCollateralBufferValue);
-  });
+      // Assert setting
+      expect(newCollateralBuffer).to.equal(newCollateralBufferValue)
+    })
 
-  it('Should not be able to update a platform setting as not a pauser', async () => {
-    // Sender address
-    const notPauser = (await ethers.getSigners())[8];
+    it('Should not be able to update a platform setting as not a pauser', async () => {
+      // Sender address
+      const { 8: notPauser } = await ethers.getSigners()
 
-    // Try to update setting
-    await settingsInstance.connect(notPauser).updatePlatformSetting(ethers.utils.formatBytes32String('CollateralBuffer'), newCollateralBufferValue).should.be.revertedWith('NOT_PAUSER');
-  });
-});
+      // Try to update setting
+      await settings
+        .connect(notPauser)
+        .updatePlatformSetting(
+          ethers.utils.formatBytes32String('CollateralBuffer'),
+          newCollateralBufferValue)
+            .should
+            .be
+            .revertedWith('NOT_PAUSER')
+    })
+  })
+
+})
