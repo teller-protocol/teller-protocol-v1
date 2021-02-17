8f2b6f57c80a6c0cd3112fabeb36703b17602cbc
Noah Passalacqua <noah@passabilities.dev>
2021-02-16 18:49:44 -0600

    Update chainlink test.

--- test/chainlinkAggregator.test.ts
+++ test/chainlinkAggregator.test.ts
@@ -4,6 +4,8 @@ import hre from 'hardhat'
 import { ChainlinkAggregator } from '../types/typechain'
 import { Network } from '../types/custom/config-types'
 import { getTokens } from '../config/tokens'
+import { getChainlink } from '../config/chainlink'
+import { NULL_ADDRESS } from '../consts'
 
 chai.should()
 chai.use(chaiAsPromised)
@@ -24,15 +26,8 @@ const setupTest = deployments.createFixture(async () => {
 
 describe('Chainlink Aggregator', async () => {
   let chainlinkAggregator: ChainlinkAggregator
+  const chainlink = getChainlink(<Network>hre.network.name)
   const tokens = getTokens(<Network>hre.network.name)
-  const sourceTokenAddress = '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9' // AAVE
-  const destinationTokenAddress = tokens.ETH // ETH
-  const aggregatorAddressToAdd = '0x6Df09E975c830ECae5bd4eD9d90f3A95a4f88012' // AAVE<>ETH
-
-  const setupAddedTest = deployments.createFixture(async () => {
-    // Add aggregator
-    await chainlinkAggregator.add(sourceTokenAddress, destinationTokenAddress, aggregatorAddressToAdd)
-  })
 
   // Setup for global tests
   beforeEach(async () => {
@@ -42,17 +37,18 @@ describe('Chainlink Aggregator', async () => {
   })
 
   describe('add', () => {
-    it('Should be able add an aggregator address a pauser', async () => {
+    const srcTokenAddress = '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9' // AAVE
+    const dstTokenAddress = tokens.ETH // ETH
+    const aggregatorAddressToAdd = '0x6Df09E975c830ECae5bd4eD9d90f3A95a4f88012' // AAVE<>ETH
+
+    it('Should be able add an aggregator address as a pauser', async () => {
       // Add aggregator
-      await chainlinkAggregator.add(sourceTokenAddress, destinationTokenAddress, aggregatorAddressToAdd)
+      await chainlinkAggregator.add(srcTokenAddress, dstTokenAddress, aggregatorAddressToAdd)
 
       // Check if aggregator was successfully added
-      const aggregatorResponse: [string, boolean] = await chainlinkAggregator.aggregatorFor(
-        sourceTokenAddress,
-        destinationTokenAddress
-      )
+      const aggregatorResponse: [string, boolean] = await chainlinkAggregator.aggregatorFor(srcTokenAddress, dstTokenAddress)
 
-      const tokenSupportResponse = await chainlinkAggregator.isTokenSupported(sourceTokenAddress)
+      const tokenSupportResponse = await chainlinkAggregator.isTokenSupported(srcTokenAddress)
 
       aggregatorResponse[0].should.be.equals(aggregatorAddressToAdd)
       tokenSupportResponse.should.be.true
@@ -63,25 +59,33 @@ describe('Chainlink Aggregator', async () => {
       const { 8: notPauser } = await ethers.getSigners()
 
       // Try to update setting
-      const fn = () =>
-        chainlinkAggregator.connect(notPauser).add(sourceTokenAddress, destinationTokenAddress, aggregatorAddressToAdd)
+      const fn = () => chainlinkAggregator.connect(notPauser).add(srcTokenAddress, dstTokenAddress, aggregatorAddressToAdd)
 
       await fn().should.be.rejectedWith('NOT_PAUSER')
     })
   })
 
   describe('remove', () => {
-    // Setup for tests
-    beforeEach(async () => {
-      await setupAddedTest()
-    })
+    const { baseTokenSym, quoteTokenSym } = chainlink[0]
+    const baseTokenAddress = tokens[baseTokenSym]
+    const quoteTokenAddress = tokens[quoteTokenSym]
 
-    it('Should be able remove an aggregator address a pauser', async () => {
+    it('Should be able remove an aggregator given a token pair as a pauser', async () => {
       // Add aggregator
-      await chainlinkAggregator['remove(address)'](sourceTokenAddress)
+      await chainlinkAggregator['remove(address,address)'](baseTokenAddress, quoteTokenAddress)
 
       // Check if aggregator was successfully removed
-      const tokenSupportResponse = await chainlinkAggregator.isTokenSupported(sourceTokenAddress)
+      const [aggregatorAddress] = await chainlinkAggregator.aggregatorFor(baseTokenAddress, quoteTokenAddress)
+
+      aggregatorAddress.should.be.equal(NULL_ADDRESS)
+    })
+
+    it('Should be able remove an aggregator address as a pauser', async () => {
+      // Add aggregator
+      await chainlinkAggregator['remove(address)'](quoteTokenAddress)
+
+      // Check if aggregator was successfully removed
+      const tokenSupportResponse = await chainlinkAggregator.isTokenSupported(quoteTokenAddress)
 
       tokenSupportResponse.should.be.false
     })
@@ -91,20 +95,19 @@ describe('Chainlink Aggregator', async () => {
       const { 8: notPauser } = await ethers.getSigners()
 
       // Try to update setting
-      const fn = () => chainlinkAggregator.connect(notPauser)['remove(address)'](sourceTokenAddress)
+      const fn = () => chainlinkAggregator.connect(notPauser)['remove(address)'](quoteTokenAddress)
 
       await fn().should.be.rejectedWith('NOT_PAUSER')
     })
   })
 
   describe('latestAnswerFor', () => {
-    // Setup for tests
-    beforeEach(async () => {
-      await setupAddedTest()
-    })
+    const { baseTokenSym, quoteTokenSym } = chainlink[0]
+    const baseTokenAddress = tokens[baseTokenSym]
+    const quoteTokenAddress = tokens[quoteTokenSym]
 
     it('Should be able get the latest price of a token pair', async () => {
-      const answer = await chainlinkAggregator.latestAnswerFor(sourceTokenAddress, destinationTokenAddress)
+      const answer = await chainlinkAggregator.latestAnswerFor(baseTokenAddress, quoteTokenAddress)
 
       answer.gt(0).should.be.true
     })
