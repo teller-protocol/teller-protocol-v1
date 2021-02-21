fad4483b77e258e572e21c6385894bac58cd5db8
sypeer <sy.peeran@gmail.com>
2021-02-21 02:46:56 -0500

    Updating loan scenario 1 with event assertions

--- test/integration/loans.test.ts
+++ test/integration/loans.test.ts
@@ -13,7 +13,7 @@ import { ERC20Detailed, Loans } from '../../types/typechain'
 chai.should()
 chai.use(solidity)
 
-const { deployments, getNamedSigner, contracts, ethers, fastForward } = hre
+const { deployments, getNamedSigner, contracts, fastForward, BN } = hre
 
 const setupTest = deployments.createFixture(async () => {
   const market = await createMarketWithLoan({
@@ -24,7 +24,7 @@ const setupTest = deployments.createFixture(async () => {
     loanType: 2,
   })
   // Forward timestamp by a day to be able to take out subsequent loans
-  await hre.fastForward(90000)
+  await fastForward(90000)
 
   return {
     ...market,
@@ -34,6 +34,7 @@ const setupTest = deployments.createFixture(async () => {
 describe('Loans', async () => {
   let market: MarketWithLoanReturn
   let borrower: Signer
+  let borrowerAddress: string
   let deployer: Signer
   let liquidator: Signer
   let lendingToken: ERC20Detailed
@@ -43,6 +44,7 @@ describe('Loans', async () => {
   beforeEach(async () => {
     // Execute snapshot and setup for tests
     borrower = await getNamedSigner('borrower')
+    borrowerAddress = await borrower.getAddress()
     deployer = await getNamedSigner('deployer')
     liquidator = await getNamedSigner('liquidator')
     market = await setupTest()
@@ -56,9 +58,35 @@ describe('Loans', async () => {
   describe('createLoanWithTerms, takeOutLoan', () => {
     // Creating a loan with terms, depositing collateral and taking out a loan successfully
     it('should be able to take out a loan with collateral', async function () {
-      const createdLoanID = (await createAndGetLoan(market, borrower, 2, hre))
-        .createdLoanId
-      createdLoanID.should.be.equals('1') // LoanID 0 was created in setupTest()
+      // Create loan
+      const loanID = await createLoan(market, 2, '2000', borrower)
+      // Get collateral required
+      const collateralNeeded = (await market.loans.getCollateralInfo(loanID))
+        .neededInCollateralTokens
+      // Deposit collateral
+      await market.loans
+        .connect(borrower)
+        .depositCollateral(borrowerAddress, loanID, collateralNeeded, {
+          value: collateralNeeded,
+        })
+        .should.emit(market.loans, 'CollateralDeposited')
+        .withArgs(loanID, borrowerAddress, collateralNeeded.toString())
+
+      await fastForward(300)
+      // Get info for created loan
+      const loanInfo = await market.loans.loans(loanID)
+
+      // Take out loan
+      await market.loans
+        .connect(borrower)
+        .takeOutLoan(loanID, BN(loanAmount, '18'))
+        .should.emit(market.loans, 'LoanTakenOut')
+        .withArgs(
+          loanID,
+          borrowerAddress,
+          loanInfo.escrow,
+          BN(loanAmount, '18')
+        )
     })
     // Creating a loan with terms and try to take out a loan without collateral unsuccessfully
     it('should not be able to take out a loan without collateral', async () => {
@@ -115,8 +143,9 @@ describe('Loans', async () => {
         .updateMaxDebtRatio(lendingToken.address, 0)
 
       // Try to take out another loan which should fail
-      const fn = () => createAndGetLoan(market, borrower, 2, hre)
-      await fn().should.be.revertedWith('SUPPLY_TO_DEBT_EXCEEDS_MAX')
+      await createAndGetLoan(market, borrower, 2, hre).should.be.revertedWith(
+        'SUPPLY_TO_DEBT_EXCEEDS_MAX'
+      )
     })
   })
 })
