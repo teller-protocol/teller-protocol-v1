579973a838db2056079c688bfff53d9d650ff27c
Jeremi G <gendronjeremi@gmail.com>
2021-03-19 01:10:22 -0400

    Missing a few provider contracts

--- scripts/truffle-contract-abis.ts
+++ scripts/truffle-contract-abis.ts
@@ -4,17 +4,31 @@ import path from 'path'
 try {
   mkdirSync(path.join(__dirname, '../new-contracts-truffle'))
 } catch (e) {}
-const base = glob.sync('base/**/*.sol/+([a-zA-Z0-9]).json', {
-  cwd: path.join(__dirname, '../artifacts/contracts'),
+const base = glob.sync('contracts/base/**/*.sol/+([a-zA-Z0-9]).json', {
+  cwd: path.join(__dirname, '../artifacts'),
 })
-const interfaces = glob.sync('interfaces/**/*.sol/+([a-zA-Z0-9]).json', {
-  cwd: path.join(__dirname, '../artifacts/contracts'),
-})
-
-base.concat(interfaces).forEach((pth) => {
+const interfaces = glob.sync(
+  'contracts/interfaces/**/*.sol/+([a-zA-Z0-9]).json',
+  {
+    cwd: path.join(__dirname, '../artifacts'),
+  }
+)
+const ERC20 = glob.sync(
+  '@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/**/*.sol/+([a-zA-Z0-9]).json',
+  {
+    cwd: path.join(__dirname, '../artifacts'),
+  }
+)
+const providers = glob.sync(
+  'contracts/providers/**/*.sol/+([a-zA-Z0-9]).json',
+  {
+    cwd: path.join(__dirname, '../artifacts'),
+  }
+)
+base.concat(interfaces, ERC20, providers).forEach((pth) => {
   const fileName = pth.split('/').pop()
   fs.writeFileSync(
     path.join(__dirname, '../new-contracts-truffle', fileName as string),
-    fs.readFileSync(path.join(__dirname, '../artifacts/contracts/', pth))
+    fs.readFileSync(path.join(__dirname, '../artifacts', pth))
   )
 })
