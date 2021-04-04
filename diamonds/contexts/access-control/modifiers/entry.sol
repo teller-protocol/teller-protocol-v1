94789576204442716b11c7ec8190baa85f520f2a
sypeer <sy.peeran@gmail.com>
2021-04-04 06:06:30 -0400

    repay update

--- diamonds/contexts/access-control/modifiers/entry.sol
+++ diamonds/contexts/access-control/modifiers/entry.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.0;
 
 import { sto_AccessControl_v2 } from "../storage.sol";
 
-abstract contract mod_entry_Domain_v1 is sto_AccessControl_v2 {
+abstract contract mod_entry_AccessControl_v1 is sto_AccessControl_v1 {
     modifier entry {
-        sto_AccessControl_v2.Layout_v2 storage layout =
-            sto_AccessControl_v2.getv2();
+        sto_AccessControl_v2.Layout_v1 storage layout =
+            sto_AccessControl_v1.getv1();
         require(layout.notEntered, "RE_ENTRANCY");
         layout.notEntered = false;
         _;
---
+++ diamonds/domains/market/internal/get-total-owed.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../storage/loans.sol";
+
+abstract contract ext_get_total_owed_v1 is sto_Loans_v1 {
+    /**
+     * @notice Returns the total amount owed for a specified loan.
+     * @param loanID The loan ID to get the total amount owed.
+     * @return uint256 The total owed amount.
+     */
+    function getTotalOwed(uint256 loanID)
+        public
+        view
+        override
+        returns (uint256)
+    {
+        if (s().loans[loanID].status == TellerCommon.LoanStatus.TermsSet) {
+            uint256 interestOwed =
+                getInterestOwedFor(
+                    loanID,
+                    s().loans[loanID].loanTerms.maxLoanAmount
+                );
+            return s().loans[loanID].loanTerms.maxLoanAmount.add(interestOwed);
+        } else if (s().loans[loanID].status == TellerCommon.LoanStatus.Active) {
+            return
+                s().loans[loanID].principalOwed.add(
+                    s().loans[loanID].interestOwed
+                );
+        }
+        return 0;
+    }
+}
+
+abstract contract ext_get_total_owed is ext_get_total_owed_v1 {}
