2576ed52f9c75f700204df90f26db1ba6eda5fc2
Jeremi G <gendronjeremi@gmail.com>
2021-04-04 00:50:56 -0400

    Main domain

--- diamonds/domains/protocol/domain.sol
+++ diamonds/domains/protocol/domain.sol
@@ -1,6 +1,15 @@
-import "./entry/asset-settings.sol";
-import "./entry/asset-settings.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-// contract ProtocolDomain is Diamond {
-//     constructor() {}
-// }
+import "../../diamond/Diamond.sol";
+import "../../contexts/access-control/internal/set-role-admin.sol";
+import "../../contexts/access-control/internal/setup-role.sol";
+
+import "./internal/roles.sol";
+
+contract TellerProtocol_v1 is Diamond {
+    constructor(
+        IDiamondCut.FacetCut[] memory diamondCut,
+        Diamond.DiamondArgs memory args
+    ) Diamond(diamondCut, args) {}
+}
