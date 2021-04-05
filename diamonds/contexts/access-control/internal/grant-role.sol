baa24990ed6040e089bccfa4d5ae64c6c8d3f2a7
Noah Passalacqua <noah@passabilities.dev>
2021-04-05 10:05:30 -0500

    Update AccessControl

--- diamonds/contexts/access-control/internal/grant-role.sol
+++ diamonds/contexts/access-control/internal/grant-role.sol
@@ -1,17 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Contracts
 import "../data.sol";
 import "./has-role.sol";
-import "./is-admin-for-role.sol";
 
 abstract contract int_grantRole_AccessControl_v1 is
     dat_AccessControl_v1,
-    int_hasRole_AccessControl_v1,
-    int_isAdminForRole_AccessControl_v1
+    int_hasRole_AccessControl_v1
 {
+    /**
+     * @dev Should only use when circumventing admin checking. See {../entry/grant-role.sol}
+     */
     function _grantRole(bytes32 role, address account) internal {
-        require(_isAdminForRole(role, msg.sender), "NOT_ADMIN");
         if (_hasRole(role, account)) return;
         accessControlRolesStore().roles[role].members[account] = true;
         emit RoleGranted(role, account, msg.sender);
--- diamonds/contexts/access-control/internal/revoke-role.sol
+++ diamonds/contexts/access-control/internal/revoke-role.sol
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Contracts
 import "../data.sol";
-import "./is-admin-for-role.sol";
+import "./has-role.sol";
 
 abstract contract int_revokeRole_AccessControl_v1 is
     dat_AccessControl_v1,
-    int_isAdminForRole_AccessControl_v1
+    int_hasRole_AccessControl_v1
 {
     function _revokeRole(bytes32 role, address account) internal {
-        require(_isAdminForRole(role, msg.sender), "AccessControl: not admin");
+        if (!_hasRole(role, account)) return;
         accessControlRolesStore().roles[role].members[account] = false;
         emit RoleRevoked(role, account, msg.sender);
     }
