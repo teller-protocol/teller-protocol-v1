d7626f25e80cb95fb1d6ee10cb3eb53475b612d8
sypeer <sy.peeran@gmail.com>
2021-05-11 22:25:47 -0400

    Aave TToken strategy for Polygon

--- contracts/escrow/dapps/libraries/LibDapps.sol
+++ contracts/escrow/dapps/libraries/LibDapps.sol
@@ -35,7 +35,7 @@ library LibDapps {
         return
             IAaveLendingPool(
                 IAaveLendingPoolAddressesProvider(
-                    0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+                    0xd05e3E715d945B59290df0ae8eF85c1BdB684744 // Polygon mainnet
                 )
                     .getLendingPool()
             ); // LP address provider contract is immutable and the address will never change
