4a860ecb0fc097893293cd5e8888e449ddf21691
Jude Dike <dikejude49@gmail.com>
2021-05-13 19:33:41 +0100

    chore: fork and use existing deployments locally

---
+++ scripts/existing.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+##
+## @author Noah Passalacqua <jude@teller.finance>
+##
+## This script is for parsing networks to fork
+##
+available_networks=('mainnet' 'rinkeby' 'kovan' 'ropsten')
+
+## Exit and display help output
+show_main_help() {
+  echo "Usage: yarn test:existing [NETWORK]"
+  echo
+  echo "Available networks:"
+  echo "  ${available_networks[*]}"
+  exit 0
+}
+script=$1
+
+## Make sure there is a network name passed
+if [[ ${available_networks[*]} =~ "$script"  ]] 
+then 
+    eval "yarn h fork $script & yarn hardhat test"
+else 
+    echo "Hardhat network not given..."
+    echo
+    show_main_help
+fi
