896dbc31f5a9c1d8607a9d0caa8c934b1cfd73c1
Jude Dike <dikejude49@gmail.com>
2021-05-14 23:08:05 +0100

    remove skip files

--- .solcover.js
+++ .solcover.js
@@ -3,7 +3,4 @@ module.exports = {
   providerOptions: {
     mnemonic: process.env.MNEMONIC_KEY,
   },
-  skipFiles: [
-    'providers/openzeppelin/SignedSafeMath.sol', // It is not included due to it is a OpenZeppelin contract.
-  ],
 }
