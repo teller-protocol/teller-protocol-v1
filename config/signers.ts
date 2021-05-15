1377bb03552e0480651f270a0921c740859e3daf
Noah Passalacqua <noah@passabilities.dev>
2021-05-15 11:23:21 -0500

    Mainnet deployment

--- config/signers.ts
+++ config/signers.ts
@@ -18,11 +18,5 @@ export const signers: Record<string, Signers> = {
   ropsten: [],
   hardhat: [],
   localhost: [],
-  mainnet: [
-    '0x0cA59Bd2255Ae40D4E1e3b939C3a97b5C9dE839b',
-    '0x53E88c675f9cb51c8CB33edf090582ac2FFDa01F',
-    '0xd380829600546D4B3b6aC5F0d9D5ED97cF799b92',
-    '0x8bCd79c2AE760E70573fCFA4e4460a7B3C8A3134',
-    '0x312a5217c12aD9b206A0380B4B134ef2b02d09A5',
-  ],
+  mainnet: [],
 }
