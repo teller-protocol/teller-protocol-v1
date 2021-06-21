4f9cd1451484cd9fb4ea1a5fb679c79d2905989b
Noah Passalacqua <noah@passabilities.dev>
2021-06-21 14:41:40 -0500

    Disable contract optimizations when running tests

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -108,7 +108,7 @@ export default <HardhatUserConfig>{
         version: '0.8.3',
         settings: {
           optimizer: {
-            enabled: true,
+            enabled: process.env.TESTING !== '1',
             runs: 200,
           },
         },
