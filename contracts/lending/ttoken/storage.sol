e9651aba6d691c55c244c25fd322daa3228a8b04
memo <majd.khoury93@gmail.com>
2021-06-28 22:14:44 -0400

    unused imports fix

--- contracts/lending/ttoken/storage.sol
+++ contracts/lending/ttoken/storage.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { ERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 struct Store {
     ERC20 underlying;
