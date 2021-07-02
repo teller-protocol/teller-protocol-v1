0aedd23a672f548d06d3ce670fba48a2a2277893
sypeer <sy.peeran@gmail.com>
2021-07-02 19:33:01 -0400

    Adding reentrancy guards to tToken operations

--- contracts/lending/ttoken/TToken_V1.sol
+++ contracts/lending/ttoken/TToken_V1.sol
@@ -2,12 +2,7 @@
 pragma solidity ^0.8.0;
 
 // Contracts
-import {
-    CONTROLLER,
-    ADMIN,
-    EXCHANGE_RATE_FACTOR,
-    ONE_HUNDRED_PERCENT
-} from "./data.sol";
+import { CONTROLLER, ADMIN, EXCHANGE_RATE_FACTOR, ONE_HUNDRED_PERCENT } from "./data.sol";
 import { ITTokenStrategy } from "./strategies/ITTokenStrategy.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
@@ -19,14 +14,11 @@ import { ITToken } from "./ITToken.sol";
 import { ICErc20 } from "../../shared/interfaces/ICErc20.sol";
 
 // Libraries
-import {
-    IERC20,
-    SafeERC20
-} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {
-    ERC165Checker
-} from "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
+import { IERC20, SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { ERC165Checker } from "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
 import { RolesLib } from "../../contexts2/access-control/roles/RolesLib.sol";
+import { ReentryMods } from "../../contexts2/access-control/reentry/ReentryMods.sol";
+import { LendingLib } from "../libraries/LendingLib.sol";
 import { NumbersLib } from "../../shared/libraries/NumbersLib.sol";
 
 // Storage
@@ -36,7 +28,7 @@ import "./storage.sol" as Storage;
  * @notice This contract represents a lending pool for an asset within Teller protocol.
  * @author develop@teller.finance
  */
-contract TToken_V1 is ITToken {
+contract TToken_V1 is ITToken, ReentryMods {
     function() pure returns (Storage.Store storage) private constant s =
         Storage.store;
 
@@ -104,12 +96,11 @@ contract TToken_V1 is ITToken {
      * @return totalSupply_ the total supply denoted in the underlying asset.
      */
     function totalUnderlyingSupply() public override returns (uint256) {
-        bytes memory data =
-            _delegateStrategy(
-                abi.encodeWithSelector(
-                    ITTokenStrategy.totalUnderlyingSupply.selector
-                )
-            );
+        bytes memory data = _delegateStrategy(
+            abi.encodeWithSelector(
+                ITTokenStrategy.totalUnderlyingSupply.selector
+            )
+        );
         return abi.decode(data, (uint256));
     }
 
@@ -220,6 +211,7 @@ contract TToken_V1 is ITToken {
         external
         override
         notRestricted
+        nonReentry(LendingLib.ID)
         returns (uint256)
     {
         require(amount > 0, "Teller: cannot mint 0");
@@ -251,7 +243,11 @@ contract TToken_V1 is ITToken {
      * @notice Redeem supplied Teller token underlying value.
      * @param amount The amount of Teller tokens to redeem.
      */
-    function redeem(uint256 amount) external override {
+    function redeem(uint256 amount)
+        external
+        override
+        nonReentry(LendingLib.ID)
+    {
         require(amount > 0, "Teller: cannot withdraw 0");
         require(
             amount <= balanceOf(_msgSender()),
@@ -273,7 +269,11 @@ contract TToken_V1 is ITToken {
      * @notice Redeem supplied underlying value.
      * @param amount The amount of underlying tokens to redeem.
      */
-    function redeemUnderlying(uint256 amount) external override {
+    function redeemUnderlying(uint256 amount)
+        external
+        override
+        nonReentry(LendingLib.ID)
+    {
         require(amount > 0, "Teller: cannot withdraw 0");
         require(
             amount <= totalUnderlyingSupply(),
@@ -425,7 +425,7 @@ contract TToken_V1 is ITToken {
 
     /**
      * @notice it retrives the value in the underlying tokens
-     * 
+     *
      */
     function _valueInUnderlying(uint256 amount, uint256 rate)
         internal
