f24703e57dd3df60f13c6da95902752925382f48
Noah Passalacqua <noah@passabilities.dev>
2021-07-04 14:12:06 -0500

    safecast the ratio to avoid future truncation of debt ratio
    Also removes unused imports for the TToken_V2 contract

--- contracts/lending/ttoken/TToken_V2.sol
+++ contracts/lending/ttoken/TToken_V2.sol
@@ -3,35 +3,13 @@ pragma solidity ^0.8.0;
 
 // Contracts
 import { TToken_V1 } from "./TToken_V1.sol";
-import {
-    CONTROLLER,
-    ADMIN,
-    EXCHANGE_RATE_FACTOR,
-    ONE_HUNDRED_PERCENT
-} from "./data.sol";
+import { CONTROLLER } from "./data.sol";
 import { ITTokenStrategy } from "./strategies/ITTokenStrategy.sol";
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-// Utils
-import { Address } from "@openzeppelin/contracts/utils/Address.sol";
-
-// Interfaces
-import { ITToken } from "./ITToken.sol";
-import { ICErc20 } from "../../shared/interfaces/ICErc20.sol";
 
 // Libraries
 import {
-    IERC20,
     SafeERC20
 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {
-    ERC165Checker
-} from "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
-import { RolesLib } from "../../contexts2/access-control/roles/RolesLib.sol";
-import { NumbersLib } from "../../shared/libraries/NumbersLib.sol";
-
-// Storage
-import "./storage.sol" as Storage;
 
 /**
  * @notice This contract represents a lending pool for an asset within Teller protocol.
