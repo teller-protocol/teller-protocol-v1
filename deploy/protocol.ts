729c552168af836e9950dfc568f10999a0aec68d
Jude Dike <dikejude49@gmail.com>
2021-07-21 20:53:48 +0100

    chore: aave claimAave solidity code

--- deploy/protocol.ts
+++ deploy/protocol.ts
@@ -129,6 +129,11 @@ const deployProtocol: DeployFunction = async (hre) => {
       args: [dappAddresses.aaveLendingPoolAddressProvider],
     },
     {
+      contract: 'AaveClaimAaveFacet',
+      skipIfAlreadyDeployed: false,
+      args: [dappAddresses.aaveIncentivesControllerAddress],
+    },
+    {
       contract: 'PoolTogetherFacet',
       skipIfAlreadyDeployed: false,
     },
