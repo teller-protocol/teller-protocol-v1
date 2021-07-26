6de2fc4d7cbf6ca4bcb49be59e1c0baff0ed2023
memo <majd.khoury93@gmail.com>
2021-07-26 10:12:30 -0400

    receiving tokens

--- contracts/nft/polygon/NFTMainnetBridgingToPolygonFacet.sol
+++ contracts/nft/polygon/NFTMainnetBridgingToPolygonFacet.sol
@@ -113,47 +113,49 @@ contract NFTMainnetBridgingToPolygonFacet is IERC1155ReceiverUpgradeable {
      *
      * This function call must use less than 30 000 gas.
      */
-    function supportsInterface(bytes4 interfaceId)
-        external
-        view
-        override(IERC165Upgradeable)
-        returns (bool)
-    {}
+    // function supportsInterface(bytes4 interfaceId)
+    //     external
+    //     view
+    //     override
+    //     returns (bool)
+    // {}
 
-    /**
-        @dev Handles the receipt of a single ERC1155 token type. This function is
-        called at the end of a `safeTransferFrom` after the balance has been updated.
-        To accept the transfer, this must return
-        `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"))`
-        (i.e. 0xf23a6e61, or its own function selector).
-        @param operator The address which initiated the transfer (i.e. msg.sender)
-        @param from The address which previously owned the token
-        @param id The ID of the token being transferred
-        @param value The amount of tokens being transferred
-        @param data Additional data with no specified format
-        @return `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"))` if transfer is allowed
-    */
+    // /**
+    //     @dev Handles the receipt of a single ERC1155 token type. This function is
+    //     called at the end of a `safeTransferFrom` after the balance has been updated.
+    //     To accept the transfer, this must return
+    //     `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"))`
+    //     (i.e. 0xf23a6e61, or its own function selector).
+    //     @param operator The address which initiated the transfer (i.e. msg.sender)
+    //     @param from The address which previously owned the token
+    //     @param id The ID of the token being transferred
+    //     @param value The amount of tokens being transferred
+    //     @param data Additional data with no specified format
+    //     @return `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"))` if transfer is allowed
+    // */
     function onERC1155Received(
         address operator,
         address from,
         uint256 id,
         uint256 value,
         bytes calldata data
-    ) external override returns (bytes4) {}
+    ) external override returns (bytes4) {
+        return IERC1155ReceiverUpgradeable.onERC1155Received.selector;
+    }
 
-    /**
-        @dev Handles the receipt of a multiple ERC1155 token types. This function
-        is called at the end of a `safeBatchTransferFrom` after the balances have
-        been updated. To accept the transfer(s), this must return
-        `bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))`
-        (i.e. 0xbc197c81, or its own function selector).
-        @param operator The address which initiated the batch transfer (i.e. msg.sender)
-        @param from The address which previously owned the token
-        @param ids An array containing ids of each token being transferred (order and length must match values array)
-        @param values An array containing amounts of each token being transferred (order and length must match ids array)
-        @param data Additional data with no specified format
-        @return `bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))` if transfer is allowed
-    */
+    // /**
+    //     @dev Handles the receipt of a multiple ERC1155 token types. This function
+    //     is called at the end of a `safeBatchTransferFrom` after the balances have
+    //     been updated. To accept the transfer(s), this must return
+    //     `bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))`
+    //     (i.e. 0xbc197c81, or its own function selector).
+    //     @param operator The address which initiated the batch transfer (i.e. msg.sender)
+    //     @param from The address which previously owned the token
+    //     @param ids An array containing ids of each token being transferred (order and length must match values array)
+    //     @param values An array containing amounts of each token being transferred (order and length must match ids array)
+    //     @param data Additional data with no specified format
+    //     @return `bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))` if transfer is allowed
+    // */
     function onERC1155BatchReceived(
         address operator,
         address from,
