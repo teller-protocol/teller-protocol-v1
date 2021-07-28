d6e504fc96045b37fbaa554d46ad4809204a5c63
Jude Dike <dikejude49@gmail.com>
2021-07-28 20:18:42 +0100

    chore: redeem staked aave

--- config/dapps.ts
+++ config/dapps.ts
@@ -2,6 +2,7 @@ import { Tokens } from '../types/custom/config-types'
 
 const mainnetDappAddresses: Tokens = {
   aaveLendingPoolAddressProvider: '0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5',
+  aaveStakeTokenAddress: '0x4da27a545c0c5b758a6ba100e3a049001de870f5',
   aaveIncentivesControllerAddress: '0xd784927Ff2f95ba542BfC824c8a8a98F3495f6b5',
   sushiswapV2RouterAddress: '0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F',
   uniswapV2RouterAddress: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
@@ -10,6 +11,7 @@ const mainnetDappAddresses: Tokens = {
 
 const polygonDappAddresses: Tokens = {
   aaveLendingPoolAddressProvider: '0xd05e3E715d945B59290df0ae8eF85c1BdB684744',
+  aaveRewardTokenAddress: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
   aaveIncentivesControllerAddress: '0x357D51124f59836DeD84c8a1730D72B749d8BC23',
   sushiswapV2RouterAddress: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506',
   uniswapV2RouterAddress: '0x0000000000000000000000000000000000000000',
--- deploy/protocol.ts
+++ deploy/protocol.ts
@@ -131,7 +131,7 @@ const deployProtocol: DeployFunction = async (hre) => {
     {
       contract: 'AaveClaimAaveFacet',
       skipIfAlreadyDeployed: false,
-      args: [dappAddresses.aaveIncentivesControllerAddress],
+      args: [dappAddresses.aaveIncentivesControllerAddress, networkName == 'polygon' ? dappAddresses.aaveRewardTokenAddress : dappAddresses.aaveStakeTokenAddress],
     },
     {
       contract: 'PoolTogetherFacet',
