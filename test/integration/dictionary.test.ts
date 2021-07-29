be201d10e3454e5b69c56c0da328b1a546a81418
andy <admazzola@gmail.com>
2021-07-29 19:19:52 -0400

    clean up

--- test/integration/dictionary.test.ts
+++ test/integration/dictionary.test.ts
@@ -30,7 +30,7 @@ chai.use(solidity)
 
 const { getNamedSigner, contracts, tokens, ethers, evm, toBN } = hre
 
-describe('NFT Dictionary z', () => {
+describe('NFT Dictionary', () => {
   getMarkets(hre.network).forEach(testLoans)
 
   function testLoans(market: Market): void {
