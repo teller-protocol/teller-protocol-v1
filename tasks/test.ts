4ac2fb96c4078bf7e250d0d4a768c2f0d80d754f
andy <admazzola@gmail.com>
2021-07-29 12:57:25 -0400

    comments

--- tasks/test.ts
+++ tasks/test.ts
@@ -27,7 +27,7 @@ task('test').setAction(async (args, hre, runSuper) => {
   // Run the actual test task
   await runSuper({
     ...args,
-    deployFixture: false,
+    // deployFixture: false,
   })
 })
 
