addd0d3baab5e707654a4cb263821ff2fb1b7b3f
andy <admazzola@gmail.com>
2021-08-01 19:08:20 -0400

    adding to tests

--- test/unit/nft-staking-v2.test.ts
+++ test/unit/nft-staking-v2.test.ts
@@ -1,27 +1,97 @@
-import { expect } from 'chai'
-import { Signer } from 'ethers'
-import hre, { ethers } from 'hardhat'
+import chai, { expect } from 'chai'
+import { solidity } from 'ethereum-waffle'
+import { BigNumber, Signer } from 'ethers'
+import hre from 'hardhat'
 
-import { TellerNFTDictionary } from '../../types/typechain'
+import { claimNFT } from '../../tasks'
+import { ITellerDiamond, TellerNFT } from '../../types/typechain'
 
-let dictionaryContract: TellerNFTDictionary
-let signerAccount: Signer
+chai.should()
+chai.use(solidity)
+
+const { getNamedSigner, contracts, tokens, ethers, evm, toBN } = hre
 
 describe('NFT Staking V2', () => {
-  it('Should deploy the dictionary', async () => {
+  // use the Deployer account to Mint a new Teller NFTv1
+
+  let deployer: Signer
+  let diamond: ITellerDiamond
+  let borrowerSigner: Signer
+  let borrower: string
+  let ownedNFTs: BigNumber[]
+  let stakedNFTs: BigNumber[]
+  let rootToken: TellerNFT
+
+  it('Should mint a new NFTv1', async () => {
     const Dictionary = await ethers.getContractFactory('TellerNFTDictionary')
 
     before(async () => {
-      /*  deployer = await hre.getNamedSigner('deployer')
+      deployer = await getNamedSigner('deployer')
+      borrower = '0x86a41524cb61edd8b115a72ad9735f8068996688'
+      borrowerSigner = (await hre.evm.impersonate(borrower)).signer
 
-      dictionaryContract = (await Dictionary.deploy()) as TellerNFTDictionary
+      // diamond and teller nft
+      diamond = await contracts.get('TellerDiamond')
+      rootToken = await contracts.get('TellerNFT')
 
-      await dictionaryContract.deployed()
+      // claim nft on behalf of the borrower
+      await claimNFT({ account: borrower, merkleIndex: 0 }, hre)
 
-      const type = await dictionaryContract.stakeableTokenType()
-      expect(type).to.equal(
-        '0x2213d707f04cdfd263a540394e2a26bbf3a63d6ad89a37f534d2ee35bdfe0d38'
-      )*/
+      // get borrower's owned nfts
+      ownedNFTs = await rootToken
+        .getOwnedTokens(borrower)
+        .then((arr) => (arr.length > 2 ? arr.slice(0, 2) : arr))
+
+      // approve transfering NFTs from the borrower to the diamond address
+      await rootToken
+        .connect(borrowerSigner)
+        .setApprovalForAll(diamond.address, true)
+    })
+  })
+
+  describe('unstakes NFTs after staking them', () => {
+    it('should stake NFTs', async () => {
+      // stake NFTs on behalf of user
+      await diamond.connect(borrowerSigner).stakeNFTs(ownedNFTs)
+
+      // get staked
+      stakedNFTs = await diamond.getStakedNFTs(borrower)
+
+      // every tokenId of the owned NFT should equate a token ID from the stakedNFT
+      for (let i = 0; i < ownedNFTs.length; i++) {
+        expect(ownedNFTs[i]).to.equal(stakedNFTs[i])
+      }
+    })
+    it('should unstake NFTs', async () => {
+      // unstake all of our staked NFTs
+      await diamond.connect(borrowerSigner).unstakeNFTs(stakedNFTs)
+
+      // retrieve our staked NFTs (should be empty)
+      stakedNFTs = await diamond.getStakedNFTs(borrower)
+
+      // we expect our staked NFTs length to be 0
+      expect(stakedNFTs.length).to.equal(0)
+    })
+  })
+  describe('unstakes NFTs from the wrong address', () => {
+    it('should stake NFTs', async () => {
+      // stake NFTs on behalf of user
+      await diamond.connect(borrowerSigner).stakeNFTs(ownedNFTs)
+
+      // get staked
+      stakedNFTs = await diamond.getStakedNFTs(borrower)
+
+      // every tokenId of the owned NFT should equate a token ID from the stakedNFT
+      for (let i = 0; i < ownedNFTs.length; i++) {
+        expect(ownedNFTs[i]).to.equal(stakedNFTs[i])
+      }
+    })
+    it('should unstake NFTs from the wrong address and fail', async () => {
+      // unstake all of our staked nfts from the wrong address
+      await diamond
+        .connect(deployer)
+        .unstakeNFTs(stakedNFTs)
+        .should.be.revertedWith('Teller: not the owner of the NFT ID!')
     })
   })
 })
