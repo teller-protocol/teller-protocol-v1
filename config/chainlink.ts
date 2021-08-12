ed83ab25dc1d14d6d6b47b9931dd9abf10be087d
sypeer <sy.peeran@gmail.com>
2021-08-12 15:33:37 -0400

    Changing WETH -> ETH for chainlink agg quoteTokenName

--- config/chainlink.ts
+++ config/chainlink.ts
@@ -3,53 +3,53 @@ import { Chainlink } from '../types/custom/config-types'
 const mainnetChainlink: Chainlink = {
   USDC_ETH: {
     baseTokenName: 'USDC',
-    quoteTokenName: 'WETH',
+    quoteTokenName: 'ETH',
     address: '0x986b5E1e1755e3C2440e960477f25201B0a8bbD4',
   },
   USDT_ETH: {
     baseTokenName: 'USDT',
-    quoteTokenName: 'WETH',
+    quoteTokenName: 'ETH',
     address: '0xEe9F2375b4bdF6387aa8265dD4FB8F16512A1d46',
   },
   DAI_ETH: {
     baseTokenName: 'DAI',
-    quoteTokenName: 'WETH',
+    quoteTokenName: 'ETH',
     address: '0x773616E4d11A78F511299002da57A0a94577F1f4',
   },
   WBTC_ETH: {
     // NOTE: 'Chainlink aggregator for BTC/WETH not WBTC/WETH',
-    baseTokenName: 'WBTC',
-    quoteTokenName: 'WETH',
+    baseTokenName: 'BTC',
+    quoteTokenName: 'ETH',
     address: '0xdeb288F737066589598e9214E782fa5A8eD689e8',
   },
   SNX_ETH: {
     baseTokenName: 'SNX',
-    quoteTokenName: 'WETH',
+    quoteTokenName: 'ETH',
     address: '0x79291A9d692Df95334B1a0B3B4AE6bC606782f8c',
   },
   MKR_ETH: {
     baseTokenName: 'MKR',
-    quoteTokenName: 'WETH',
+    quoteTokenName: 'ETH',
     address: '0x24551a8Fb2A7211A25a17B1481f043A8a8adC7f2',
   },
   YFI_ETH: {
     baseTokenName: 'YFI',
-    quoteTokenName: 'WETH',
+    quoteTokenName: 'ETH',
     address: '0x7c5d4F8345e66f68099581Db340cd65B078C41f4',
   },
   AAVE_ETH: {
     baseTokenName: 'AAVE',
-    quoteTokenName: 'WETH',
+    quoteTokenName: 'ETH',
     address: '0x6Df09E975c830ECae5bd4eD9d90f3A95a4f88012',
   },
   LINK_ETH: {
     baseTokenName: 'LINK',
-    quoteTokenName: 'WETH',
+    quoteTokenName: 'ETH',
     address: '0xDC530D9457755926550b59e8ECcdaE7624181557',
   },
   COMP_ETH: {
     baseTokenName: 'COMP',
-    quoteTokenName: 'WETH',
+    quoteTokenName: 'ETH',
     address: '0x1B39Ee86Ec5979ba5C322b826B3ECb8C79991699',
   },
 }
@@ -94,15 +94,15 @@ const polygonChainlink: Chainlink = {
 
 export const chainlink: Record<string, Chainlink> = {
   kovan: {
-    ETH_USDC: {
-      baseTokenName: 'WETH',
-      quoteTokenName: 'USDC',
-      address: '0x9326BFA02ADD2366b30bacB125260Af641031331',
+    USDC_ETH: {
+      baseTokenName: 'USDC',
+      quoteTokenName: 'ETH',
+      address: '0x64EaC61A2DFda2c3Fa04eED49AA33D021AeC8838',
     },
-    ETH_DAI: {
-      baseTokenName: 'WETH',
-      quoteTokenName: 'DAI',
-      address: '0x9326BFA02ADD2366b30bacB125260Af641031331',
+    DAI_ETH: {
+      baseTokenName: 'DAI',
+      quoteTokenName: 'ETH',
+      address: '0x22B58f1EbEDfCA50feF632bD73368b2FdA96D541',
     },
     LINK_DAI: {
       baseTokenName: 'LINK',
@@ -117,12 +117,12 @@ export const chainlink: Record<string, Chainlink> = {
   },
   rinkeby: {
     ETH_USDC: {
-      baseTokenName: 'WETH',
+      baseTokenName: 'ETH',
       quoteTokenName: 'USDC',
       address: '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e',
     },
     ETH_DAI: {
-      baseTokenName: 'WETH',
+      baseTokenName: 'ETH',
       quoteTokenName: 'DAI',
       address: '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e',
     },
@@ -140,12 +140,12 @@ export const chainlink: Record<string, Chainlink> = {
   ropsten: {
     USDC_ETH: {
       baseTokenName: 'USDC',
-      quoteTokenName: 'WETH',
+      quoteTokenName: 'ETH',
       address: '0xE48d431DB6cdf5E361c554fA95826a44A27d1167',
     },
     DAI_ETH: {
       baseTokenName: 'DAI',
-      quoteTokenName: 'WETH',
+      quoteTokenName: 'ETH',
       address: '0xd63bAd023755e15c7B4ce99dB8B71100A0736d43',
     },
     LINK_DAI: {
