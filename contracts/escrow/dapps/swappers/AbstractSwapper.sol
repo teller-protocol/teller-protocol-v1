cb2d42a08ba22f2459f3f17b3762bd5974efbbf1
Syed <sy.peeran@gmail.com>
2021-11-24 14:03:00 -0500

    Checking for wrapped native token

--- contracts/escrow/dapps/swappers/AbstractSwapper.sol
+++ contracts/escrow/dapps/swappers/AbstractSwapper.sol
@@ -13,11 +13,14 @@ import { AppStorageLib } from "../../../storage/app.sol";
 
 abstract contract AbstractSwapper is PausableMods, DappMods {
     function __isValidPath(address src, address dst) internal returns (bool) {
-        ChainlinkPricer chainlinkPricer = AppStorageLib.store()
+        ChainlinkPricer chainlinkPricer = AppStorageLib
+            .store()
             .priceAggregator
             .chainlinkPricer();
         return
-            address(chainlinkPricer.getEthAggregator(src)) != address(0) &&
+            src == AppStorageLib.store().wrappedNativeToken ||
+            (address(chainlinkPricer.getEthAggregator(src)) != address(0) &&
+                dst == AppStorageLib.store().wrappedNativeToken) ||
             address(chainlinkPricer.getEthAggregator(dst)) != address(0);
     }
 }
